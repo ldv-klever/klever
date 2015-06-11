358d552e1ba34c0f01f085a3ec28f044ddd57438
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2015-06-11 18:32:29 +0300

    Add settings for db, add new applizations, add bootstrap static files.

---
+++ Omega/Omega/Omega/db_settings.py
@@ -0,0 +1,11 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
