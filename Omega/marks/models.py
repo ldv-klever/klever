7a92f4fdb4bb93e02606b4207e55856a4835e0fd
Vitaly Mordan <mordan@ispras.ru>
2015-06-26 19:10:13 +0300

    Added basic class for marks.

--- Omega/marks/models.py
+++ Omega/marks/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+class Mark(models.Model):
+    id = models.AutoField(primary_key=True)
+    identifier = models.CharField(max_length=255)
+    format = models.PositiveSmallIntegerField()
+    version = models.PositiveSmallIntegerField()
+    # TODO: author_id
+
+    CLASS = (
+        ('0', _('linux kernel modules')),
+        ('1', _('linux kernel git repository commits')),
+        ('2', _('c programs')),
+    )
+    job_class = models.CharField(max_length=1, choices=CLASS, default='0')
+    
+    STATUS = (
+        ('0', _('Unreported')),
+        ('1', _('Reported')),
+        ('2', _('Fixed')),
+        ('3', _('Rejected')),
+    )
+    status = models.CharField(max_length=1, choices=STATUS, default='0')
+    
+    is_modifiable = models.BooleanField()
+    change_date = models.DateTimeField()
+    comment = models.TextField(null=True)
+
+    def __str__(self):
+        return self.id
+
+
