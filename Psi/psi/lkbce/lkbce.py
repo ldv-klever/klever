c77f4c2f98f87d4320c8f7bb851e03a4a256d00a
Evgeny Novikov <novikov@ispras.ru>
2015-08-10 13:45:29 +0300

    More proper extracting of Linux kernel sources

--- Psi/psi/lkbce/lkbce.py
+++ Psi/psi/lkbce/lkbce.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import shutil
 import tarfile
 import urllib.parse
 
@@ -13,20 +14,22 @@ class Component(psi.component.ComponentBase):
         self.logger.info('Prepare Linux kernel working source tree "linux"')
 
         linux_kernel_src = self.conf['Linux kernel']['src']
+        linux_kernel_work_src_tree = os.path.join(self.conf['root id'], 'linux')
 
-        try:
-            o = urllib.parse.urlparse(linux_kernel_src)
-            raise NotImplementedError('Can not download Linux kernel "{0}"'.format(linux_kernel_src))
-        except:
-            self.logger.debug('Linux kernel is not provided in form of URL')
+        o = urllib.parse.urlparse(linux_kernel_src)
+        if o[0] in ('http', 'https', 'ftp'):
+            raise NotImplementedError('Linux kernel is likely provided in unsopported form of remote archive')
+        elif o[0] == 'git':
+            raise NotImplementedError('Linux kernel is likely provided in unsopported form of Git repository')
+        elif o[0]:
+            raise ValueError('Linux kernel is provided in unsupported form "{0}"'.format(o[0]))
 
-        linux_kernel_src = psi.utils.find_file(self.logger, self.conf['root id'], linux_kernel_src)
+        linux_kernel_src = psi.utils.find_file_or_dir(self.logger, self.conf['root id'], linux_kernel_src)
 
         if os.path.isdir(linux_kernel_src):
             self.logger.debug('Linux kernel is provided in form of source tree')
+            shutil.copytree(linux_kernel_src, linux_kernel_work_src_tree)
         elif os.path.isfile(linux_kernel_src):
             self.logger.debug('Linux kernel is provided in form of archive')
-            #with tarfile.open(linux_kernel_src) as TarFile:
-            #    TarFile.extractall(os.path.join(self.conf['root id'], 'linux'))
-        else:
-            raise ValueError('Linux kernel is provided in unsupported form')
+            with tarfile.open(linux_kernel_src) as TarFile:
+                TarFile.extractall(linux_kernel_work_src_tree)
