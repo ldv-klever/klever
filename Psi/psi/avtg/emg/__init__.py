64c96eec8f11bcb65ed141e02759116e45a7b9e7
Ilja Zakharov <ilja.zakharov@ispras.ru>
2015-11-17 18:57:24 +0300

    Add todos for emg plugin.

--- Psi/psi/avtg/emg/__init__.py
+++ Psi/psi/avtg/emg/__init__.py
@@ -6,7 +6,21 @@ import psi.utils
 
 class EMG(psi.components.Component):
     def generate_environment(self):
-        self.logger.info("Prepare environment model for an abstract verification task {}".format(self.id))
+        self.logger.info("Start environment model generator instance {}".format(self.id))
+        # TODO: Check existanc of all necessary configuration files
+
+        # TODO: Import interface categories configuration
+
+        # TODO: Generate aspect files
+
+        # TODO: Start CIF for each C file with generated aspect and read outut
+
+        # TODO: Can we do it in parallel
+
+        # TODO: Read output and parse CIF out
+
+        # TODO: Generate module interface specification
+
         self.__check_specifications()
 
 
