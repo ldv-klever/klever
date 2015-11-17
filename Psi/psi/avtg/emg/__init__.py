5a9d0c3fe9297ff2319ea4eff7ca19eca9c07232
Evgeny Novikov <novikov@ispras.ru>
2015-11-17 19:29:07 +0300

    Fix broken AVTG plugin

--- Psi/psi/avtg/emg/__init__.py
+++ Psi/psi/avtg/emg/__init__.py
@@ -7,8 +7,5 @@ import psi.utils
 class EMG(psi.components.Component):
     def generate_environment(self):
         self.logger.info("Prepare environment model for an abstract verification task {}".format(self.id))
-        self.__check_specifications()
-
-
 
     main = generate_environment
