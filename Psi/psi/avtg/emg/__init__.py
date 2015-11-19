e1e9ff8d5ab3ac8ffb64f8fbd5b5e6a7e8090fdb
Evgeny Novikov <novikov@ispras.ru>
2015-11-19 20:39:01 +0300

    Add receiving and sending of abstract task decription to all plugins

--- Psi/psi/avtg/emg/__init__.py
+++ Psi/psi/avtg/emg/__init__.py
@@ -7,7 +7,7 @@ import psi.utils
 class EMG(psi.components.Component):
     def generate_environment(self):
         self.logger.info("Prepare environment model for an abstract verification task {}".format(self.id))
-        print(self.mqs['abstract task description'].get())
-        # FIXME: self.__check_specifications()
+        abstract_task_desc = self.mqs['abstract task description'].get()
+        self.mqs['abstract task description'].put(abstract_task_desc)
 
     main = generate_environment
--- Psi/psi/avtg/ri/__init__.py
+++ Psi/psi/avtg/ri/__init__.py
@@ -6,6 +6,7 @@ import psi.utils
 
 class RI(psi.components.Component):
     def do_something_useful(self):
-        pass
+        abstract_task_desc = self.mqs['abstract task description'].get()
+        self.mqs['abstract task description'].put(abstract_task_desc)
 
     main = do_something_useful
