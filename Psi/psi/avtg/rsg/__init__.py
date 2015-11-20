95804b256390af65a19c3f47781b735666a0c31a
Evgeny Novikov <novikov@ispras.ru>
2015-11-20 12:26:20 +0300

    Add models to abstract task description

--- Psi/psi/avtg/rsg/__init__.py
+++ Psi/psi/avtg/rsg/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import json
 import os
 
 import psi.components
@@ -11,6 +12,7 @@ class RSG(psi.components.Component):
         self.abstract_task_desc = self.mqs['abstract task description'].get()
 
         self.add_aspects()
+        self.add_models()
 
         self.mqs['abstract task description'].put(self.abstract_task_desc)
 
@@ -35,3 +37,48 @@ class RSG(psi.components.Component):
                     cc_extra_full_desc_file['plugin aspect files'] = []
                     cc_extra_full_desc_file['plugin aspect files'].append(
                         {"plugin": self.name, "aspect files": aspects})
+
+    def add_models(self):
+        self.logger.info("Add models to abstract task description")
+
+        # Get common and rule specific models.
+        models = []
+
+        for model in self.conf['common models'] + self.conf['models']:
+            # All models are relative to models directory.
+            model = psi.utils.find_file_or_dir(self.logger, self.conf['main working directory'],
+                                               os.path.join(self.conf['models directory'], model))
+            self.logger.debug('Get model "{0}"'.format(model))
+            models.append(model)
+
+        # CC extra full description files will be put to this directory as well as corresponding output files.
+        os.makedirs('models')
+
+        # Generate CC extra full description file per each model and add it to abstract task description.
+        for grp in self.abstract_task_desc['grps']:
+            for model in models:
+                out_file = os.path.join('models', '{}.o'.format(os.path.splitext(os.path.basename(model))[0]))
+                full_desc_file = '{0}.json'.format(out_file)
+                with open(full_desc_file, 'w') as fp:
+                    json.dump({
+                        # Input file path should be relative to source tree root since compilation options are relative
+                        # to this directory and we will change directory to that one before invoking preprocessor.
+                        "in files": [os.path.relpath(model, self.conf['source tree root'])],
+                        # Otput file should be located somewhere inside RSG working directory to avoid races.
+                        "out file": os.path.relpath(out_file, self.conf['source tree root']),
+                        "opts": [
+                            # First of all header files specific for rule specifications will be searched for.
+                            ["-I{0}".format(os.path.relpath(
+                                psi.utils.find_file_or_dir(self.logger, self.conf['main working directory'],
+                                                           self.conf['rule specifications directory']),
+                                self.conf['source tree root']))] +
+                            self.conf['model CC opts']
+                        ]
+                    }, fp, sort_keys=True, indent=4)
+                self.logger.debug('CC extra full description file is "{0}"'.format(full_desc_file))
+
+                grp['cc extra full desc files'].append({
+                    'cc full desc file': os.path.relpath(full_desc_file, self.conf['source tree root']),
+                    'rule spec id': self.conf['rule spec id'],
+                    'bug kinds': self.conf['bug kinds']
+                })
