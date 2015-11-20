76dd8cd7333d1160ed9dd7cb6ba6a69cc6147be2
Evgeny Novikov <novikov@ispras.ru>
2015-11-20 13:00:41 +0300

    Make proper relative paths
    We are going to change directory to working source tree root, so make relative paths relative that directory. Besides resolve symlinks.

--- Psi/psi/avtg/rsg/__init__.py
+++ Psi/psi/avtg/rsg/__init__.py
@@ -36,7 +36,9 @@ class RSG(psi.components.Component):
                 if 'plugin aspect files' not in cc_extra_full_desc_file:
                     cc_extra_full_desc_file['plugin aspect files'] = []
                     cc_extra_full_desc_file['plugin aspect files'].append(
-                        {"plugin": self.name, "aspect files": aspects})
+                        {"plugin": self.name,
+                         "aspect files": [os.path.relpath(aspect, os.path.realpath(self.conf['source tree root'])) for
+                                          aspect in aspects]})
 
     def add_models(self):
         self.logger.info("Add models to abstract task description")
@@ -63,22 +65,23 @@ class RSG(psi.components.Component):
                     json.dump({
                         # Input file path should be relative to source tree root since compilation options are relative
                         # to this directory and we will change directory to that one before invoking preprocessor.
-                        "in files": [os.path.relpath(model, self.conf['source tree root'])],
+                        "in files": [os.path.relpath(model, os.path.realpath(self.conf['source tree root']))],
                         # Otput file should be located somewhere inside RSG working directory to avoid races.
-                        "out file": os.path.relpath(out_file, self.conf['source tree root']),
+                        "out file": os.path.relpath(out_file, os.path.realpath(self.conf['source tree root'])),
                         "opts": [
                             # First of all header files specific for rule specifications will be searched for.
                             ["-I{0}".format(os.path.relpath(
                                 psi.utils.find_file_or_dir(self.logger, self.conf['main working directory'],
                                                            self.conf['rule specifications directory']),
-                                self.conf['source tree root']))] +
+                                os.path.realpath(self.conf['source tree root'])))] +
                             self.conf['model CC opts']
                         ]
                     }, fp, sort_keys=True, indent=4)
                 self.logger.debug('CC extra full description file is "{0}"'.format(full_desc_file))
 
                 grp['cc extra full desc files'].append({
-                    'cc full desc file': os.path.relpath(full_desc_file, self.conf['source tree root']),
+                    'cc full desc file': os.path.relpath(full_desc_file,
+                                                         os.path.realpath(self.conf['source tree root'])),
                     'rule spec id': self.conf['rule spec id'],
                     'bug kinds': self.conf['bug kinds']
                 })
