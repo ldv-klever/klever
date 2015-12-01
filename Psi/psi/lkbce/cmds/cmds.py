bd7513f30fef27ae7e5fd39832873d700684d3ef
Evgeny Novikov <novikov@ispras.ru>
2015-12-01 15:53:54 +0300

    Exclude unsupported options from all invocations of CIF/Aspectator
    Before they were excluded just from invocations for building the Linux kernel.

--- Psi/psi/lkbce/cmds/cmds.py
+++ Psi/psi/lkbce/cmds/cmds.py
@@ -16,19 +16,23 @@ class Command:
         self.opts = argv[1:]
 
     def launch(self):
-        with psi.utils.LockedOpen(os.environ['LINUX_KERNEL_RAW_BUILD_CMDS_FILE'], 'a') as fp:
-            fp.write('{0}\n{1}'.format('\n'.join([self.cmd.upper() if self.cmd != 'gcc' else 'CC'] + self.opts),
-                                       self.cmds_separator))
-
-        # Eclude path where wrapper build command is located.
-        os.environ['PATH'] = re.sub(r'^[^:]+:', '', os.environ['PATH'])
-
         # TODO: remove these ugly workarounds after updating Aspectator to one of the newest version of GCC.
         if self.cmd == 'gcc':
             for unsupported_opt in ('-Werror=date-time', '-mpreferred-stack-boundary=3', '-Wno-maybe-uninitialized',
                                     '--param=allow-store-data-races=0'):
                 if unsupported_opt in self.opts:
                     self.opts.remove(unsupported_opt)
+
+        # TODO: replacement of GCC with CC is incorrect in general case since GCC can accept several input files,
+        # compile and link them together. But there is the only example when this happens when complete build of the
+        # Linux kernel, and corresponding object file isn't linked to any module. More proper implementation is to
+        # replace GCC with CC and LD if it is necessary.
+        with psi.utils.LockedOpen(os.environ['LINUX_KERNEL_RAW_BUILD_CMDS_FILE'], 'a') as fp:
+            fp.write('{0}\n{1}'.format('\n'.join([self.cmd.upper() if self.cmd != 'gcc' else 'CC'] + self.opts),
+                                       self.cmds_separator))
+
+        # Eclude path where wrapper build command is located.
+        os.environ['PATH'] = re.sub(r'^[^:]+:', '', os.environ['PATH'])
 
         # Execute original build command.
         subprocess.call(tuple(['aspectator' if self.cmd == 'gcc' else self.cmd] + self.opts))
