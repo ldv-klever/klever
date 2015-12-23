5369f4876a1b501f5a616f5f00890e8cbd5581d6
vladimir <gratinskiy@ispras.ru>
2015-12-23 14:21:43 +0300

    Fix settings for new django versions.

--- bridge/bridge/development.py
+++ bridge/bridge/development.py
@@ -49,6 +49,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'debug': DEBUG
         },
     },
 ]
