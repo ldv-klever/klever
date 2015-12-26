2e77cef71c36136726378b9a60e7955f6d1f9fec
Evgeny Novikov <novikov@ispras.ru>
2015-12-26 17:24:59 +0300

    Fix model comments

--- bridge/jobs/presets/linux-3.14/rule specs/linux/err.c
+++ bridge/jobs/presets/linux-3.14/rule specs/linux/err.c
@@ -3,14 +3,14 @@
 /* MODEL_FUNC_DEF Check whether a given pointer represents an error */
 long ldv_is_err(const void *ptr)
 {
-	/* RETURN Zero if a given pointer doesn't represent an error and non zero otherwise */
+	/* RETURN Zero if pointer doesn't represent an error and non zero otherwise */
 	return ((unsigned long)ptr > LDV_PTR_MAX);
 }
 
-/* MODEL_FUNC_DEF Convert a given error error to a pointer */
+/* MODEL_FUNC_DEF Convert a given error to a pointer */
 void *ldv_err_ptr(long error)
 {
-	/* RETURN Pointer representation of a given error */
+	/* RETURN Pointer representation of error */
 	return (void *)(LDV_PTR_MAX - error);
 }
 
@@ -24,6 +24,6 @@ long ldv_ptr_err(const void *ptr)
 /* MODEL_FUNC_DEF Check whether a given pointer represents an error or it is NULL */
 long ldv_is_err_or_null(const void *ptr)
 {
-	/* RETURN Zero if a given pointer doesn't represents an error or it isn't NULL and non zero otherwise */
+	/* RETURN Zero if pointer doesn't represents an error and it isn't NULL and non zero otherwise */
 	return !ptr || ldv_is_err((unsigned long)ptr);
 }
