86d1e08a8c4ccb8b261616cf8f5f8db0516a4df8
Evgeny Novikov <novikov@ispras.ru>
2016-01-20 17:02:50 +0300

    Synchronize preset jobs

--- bridge/jobs/presets/validation/rule specs/linux/usb2.c
+++ bridge/jobs/presets/validation/rule specs/linux/usb2.c
@@ -1,47 +1,27 @@
 #include <linux/kernel.h>
 #include <verifier/rcv.h>
 
-/* There are 3 possible model states. */
+/* There are 2 possible model states. */
 enum
 {
 	LDV_PROBE_ZERO_STATE = 0, /* No error occured. */
-	LDV_PROBE_ERROR = 1,      /* Error occured. probe() should return error an code (or at least not zero). */
+	LDV_PROBE_ERROR = 1,      /* Error occured. probe() should return error code (or at least not zero). */
 };
 
-/* OTHER The model automaton state (one of thee possible ones) */
+/* CHANGE_STATE Model automaton state (one of two possible ones) */
 int ldv_probe_state = LDV_PROBE_ZERO_STATE;
 
-/* MODEL_FUNC_DEF Non-deterministically change state after call to usb_register */
+/* MODEL_FUNC_DEF Nondeterministically change state after call to usb_register() */
 int ldv_usb_register(void)
 {
 	int nondet;
 
-	/* LDV_COMMENT_OTHER Nondeterministically report an error */
-	if (nondet < 0)
-	{
-		/* LDV_COMMENT_CHANGE_STATE Error occured */
-		ldv_probe_state = LDV_PROBE_ERROR;
-		/* LDV_COMMENT_RETURN Return an error */
-		return nondet;
-	}
-	else if (nondet >= 0)
-	{
-		/* LDV_COMMENT_RETURN Assume no error occured */
-		return 0;
-	}
-}
-
-/* MODEL_FUNC_DEF Non-deterministically change state after call to register_netdev */
-int ldv_register_netdev(void)
-{
-	int nondet;
-
-	/* OTHER Nondeterministically report an error */
+	/* OTHER Nondeterministically report error */
 	if (nondet < 0)
 	{
 		/* CHANGE_STATE Error occured */
 		ldv_probe_state = LDV_PROBE_ERROR;
-		/* RETURN Return an error */
+		/* RETURN Return error code */
 		return nondet;
 	}
 	else if (nondet >= 0)
@@ -51,11 +31,32 @@ int ldv_register_netdev(void)
 	}
 }
 
-/* MODEL_FUNC_DEF Check the error code was properly propagated in probe */
+/* MODEL_FUNC_DEF Nondeterministically change state after call to register_netdev() */
+int ldv_register_netdev(void)
+{
+	int nondet;
+
+	/* OTHER Nondeterministically report error */
+	if (nondet < 0)
+	{
+		/* CHANGE_STATE Error occured */
+		ldv_probe_state = LDV_PROBE_ERROR;
+		/* RETURN Return error code */
+		return nondet;
+	}
+	else if (nondet >= 0)
+	{
+		/* RETURN Assume no error occured */
+		return 0;
+	}
+}
+
+/* MODEL_FUNC_DEF Check that error code was properly propagated in probe() */
 void ldv_check_return_value_probe(int retval)
 {
 	if (ldv_probe_state == LDV_PROBE_ERROR)
 	{
+		/* ASSERT Errors of usb_register() and register_netdev() should be properly propagated */
 		ldv_assert(retval != 0);
 	}
 }
