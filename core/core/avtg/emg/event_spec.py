32ff694d8e333563770f5781a8b4b0adc9def887
Ilja Zakharov <ilja.zakharov@ispras.ru>
2016-01-21 13:32:07 +0300

    Call exit before deregistration in EMG

--- core/core/avtg/emg/event_spec.py
+++ core/core/avtg/emg/event_spec.py
@@ -133,9 +133,13 @@ class EventModel:
         stop.statements = ["ldv_check_final_state();"]
         self.model["entry"].subprocesses['stop'] = stop
 
+        none = Subprocess('none', {})
+        none.type = "condition"
+        self.model["entry"].subprocesses['none'] = none
+
         # Add subprocesses finally
-        self.model["entry"].process = "[init].(ret_init).(<init_failed>.<stop> | <init_success>.{}.[exit].<stop>)".\
-                                      format('.'.join(dispatches))
+        self.model["entry"].process = "[init].(ret_init).(<init_failed>.<stop> | <init_success>.({} | <none>).[exit]." \
+                                      "<stop>)".format('.'.join(dispatches))
         self.model["entry"].process_ast = process_parse(self.model["entry"].process)
 
     def __select_processes_and_models(self):
