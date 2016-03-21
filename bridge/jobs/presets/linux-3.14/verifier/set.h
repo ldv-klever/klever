92616491e123de76acf5bde0cd654e59fa5e4d98
Evgeny Novikov <novikov@ispras.ru>
2016-03-21 12:53:24 +0300

    Support several models of sets and specify the default one for all rule specifications

--- bridge/jobs/presets/linux-3.14/verifier/set.h
+++ bridge/jobs/presets/linux-3.14/verifier/set.h
@@ -1,47 +1,32 @@
 #ifndef _LDV_SET_H_
 #define _LDV_SET_H_
 
-/* At the moment sets are represented just as counters, but this won't be the
- * case in future. */
-
-/* Neither BLAST (with and without aliases), nor CPAchecker can't work with
- * even such a simple form of sets, since they don't support aliases.
-typedef int *Set;
+#if defined(LDV_SETS_MODEL_FLAG)
+typedef _Bool Set;
 typedef void *Element;
-
-static inline void ldv_set_init(Set set)
-{
-	*set = 0;
-}
-
-static inline void ldv_set_add(Set set, Element element)
-{
-	(*set)++;
-}
-
-static inline void ldv_set_remove(Set set, Element element)
-{
-	(*set)--;
-}
-
-static inline int ldv_set_contains(Set set, Element element)
-{
-	return *set != 0;
-}
-
-static inline int ldv_set_is_empty(Set set)
-{
-	return *set == 0;
-}
-*/
-
+# define ldv_set_init(set) (set = 0)
+# define ldv_set_add(set, element) (set = 1)
+# define ldv_set_remove(set, element) (set = 0)
+# define ldv_set_contains(set, element) (set == 1)
+# define ldv_set_is_empty(set) (set == 0)
+#elif defined(LDV_SETS_MODEL_COUNTER)
 typedef int Set;
 typedef void *Element;
-
-#define ldv_set_init(set) (set = 0)
-#define ldv_set_add(set, element) (set++)
-#define ldv_set_remove(set, element) (set--)
-#define ldv_set_contains(set, element) (set != 0)
-#define ldv_set_is_empty(set) (set == 0)
+# define ldv_set_init(set) (set = 0)
+# define ldv_set_add(set, element) (set++)
+# define ldv_set_remove(set, element) (set--)
+# define ldv_set_contains(set, element) (set != 0)
+# define ldv_set_is_empty(set) (set == 0)
+#elif defined(LDV_SETS_MODEL_NONNEGATIVE_COUNTER)
+typedef unsigned int Set;
+typedef void *Element;
+# define ldv_set_init(set) (set = 0)
+# define ldv_set_add(set, element) (set++)
+# define ldv_set_remove(set, element) ((set == 0) ?: set--)
+# define ldv_set_contains(set, element) (set != 0)
+# define ldv_set_is_empty(set) (set == 0)
+#else
+# error "Sets are likely used but sets model is not specified"
+#endif
 
 #endif /* _LDV_SET_H_ */
