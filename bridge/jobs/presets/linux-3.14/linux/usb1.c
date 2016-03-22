f9999ecb8347e858b5b0b8246148211b74932e52
Evgeny Novikov <novikov@ispras.ru>
2016-03-22 19:19:37 +0300

    Fix linux:usb1 rule specification

--- bridge/jobs/presets/linux-3.14/linux/usb1.c
+++ bridge/jobs/presets/linux-3.14/linux/usb1.c
@@ -1,31 +1,42 @@
+#include <linux/usb.h>
 #include <verifier/rcv.h>
+#include <verifier/map.h>
 
-/* There are 2 possible states of USB device reference counters. */
-enum
+Map LDV_USB_DEV_REF_COUNTS;
+
+/* MODEL_FUNC_DEF Increment USB device reference counter */
+void ldv_usb_get_dev(struct usb_device *dev)
 {
-	LDV_USB_DEV_ZERO_STATE = 0, /* USB device reference hasn't been acquired. */
-	LDV_USB_DEV_INCREASED = 1   /* USB device reference counter increased. */
-};
+    /* OTHER Whether USB device is not NULL */
+    if (dev) {
+	    /* CHANGE_STATE Increment USB device reference counter */
+        ldv_map_put(LDV_USB_DEV_REF_COUNTS, dev,
+                    ldv_map_contains_key(LDV_USB_DEV_REF_COUNTS, dev)
+                    ? ldv_map_get(LDV_USB_DEV_REF_COUNTS, dev) + 1
+                    : 1);
+    }
 
-/* OTHER Model automaton state (one of two possible ones) */
-int ldv_usb_dev_state = LDV_USB_DEV_ZERO_STATE;
-
-/* MODEL_FUNC_DEF Change state after increasing reference counter with usb_get_dev() */
-void ldv_usb_get_dev(void)
-{
-	/* CHANGE_STATE Increase reference counter */
-	ldv_usb_dev_state++;
+	/* RETURN USB device */
+	return dev;
 }
 
-/* MODEL_FUNC_DEF Check that USB device reference counter has been increased and change state after decreasing reference counter with usb_put_dev() */
-void ldv_usb_put_dev(void)
+/* MODEL_FUNC_DEF Check that USB device reference counter was incremented and decrement it */
+void ldv_usb_put_dev(struct usb_device *dev)
 {
-	/* ASSERT USB device reference counter should be increased */
-	ldv_assert(ldv_usb_dev_state >= LDV_USB_DEV_INCREASED);
-	/* CHANGE_STATE Decrease reference counter */
-	ldv_usb_dev_state--;
+    /* OTHER Whether USB device is not NULL */
+    if (dev) {
+        /* ASSERT USB device reference counter must be incremented */
+        ldv_assert("linux:usb:resource:ref:non-incremented counter decrement", ldv_map_contains_key(LDV_USB_DEV_REF_COUNTS, dev));
+        /* ASSERT USB device reference counter must be incremented */
+        ldv_assert("linux:usb:resource:ref:less initial decrement", ldv_map_get(LDV_USB_DEV_REF_COUNTS, dev) > 0);
+        /* CHANGE_STATE Decrement USB device reference counter */
+        ldv_map_get(LDV_USB_DEV_REF_COUNTS, dev) > 1
+            ? ldv_map_put(LDV_USB_DEV_REF_COUNTS, dev, ldv_map_get(LDV_USB_DEV_REF_COUNTS, dev) - 1)
+            : ldv_map_remove(LDV_USB_DEV_REF_COUNTS, dev);
+	}
 }
 
+/* TODO: EMG doesn't support this now. */
 /* MODEL_FUNC_DEF Check that probe() keeps model in proper state */
 void ldv_check_return_value_probe(int retval)
 {
@@ -33,13 +44,20 @@ void ldv_check_return_value_probe(int retval)
 	if (retval)
 	{
 		/* ASSERT USB device reference counter should not be increased */
-		ldv_assert(ldv_usb_dev_state < LDV_USB_DEV_INCREASED);
+		ldv_assert("", ldv_map_is_empty(LDV_USB_DEV_REF_COUNTS));
 	}
 }
 
-/* MODEL_FUNC_DEF Check that USB device reference isn't acquired at the end */
+/* MODEL_FUNC_DEF Initialize all USB device reference counters at the beginning */
+void ldv_initialize(void)
+{
+	/* CHANGE_STATE All USB device reference counters aren't incremented at the beginning */
+	ldv_map_init(LDV_USB_DEV_REF_COUNTS);
+}
+
+/* MODEL_FUNC_DEF Check that all USB device reference counters are not incremented at the end */
 void ldv_check_final_state(void)
 {
-	/* ASSERT Acquired USB device reference should be released before finishing operation */
-	ldv_assert(ldv_usb_dev_state < LDV_USB_DEV_INCREASED);
+	/* ASSERT All incremented USB device reference counters must be decremented at the end */
+	ldv_assert("linux:usb:resource:ref:more initial at exit", ldv_map_is_empty(LDV_USB_DEV_REF_COUNTS));
 }
