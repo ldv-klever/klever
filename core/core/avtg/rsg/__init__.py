a6c79db363fceb4a9de380a67b255b35c6333bd9
Evgeny Novikov <novikov@ispras.ru>
2016-03-22 18:01:07 +0300

    Implement advanced rule specification models
    In addition to C files one is able to specify additional settings for models, e.g. a sets model (now), bit accuracy (in future) and so on.

--- core/core/avtg/rsg/__init__.py
+++ core/core/avtg/rsg/__init__.py
@@ -13,7 +13,7 @@ class RSG(core.components.Component):
         self.abstract_task_desc = self.mqs['abstract task description'].get()
 
         aspects = []
-        models = []
+        models = {}
 
         if 'files' in self.abstract_task_desc:
             self.logger.info('Get additional aspects and models specified in abstract task description')
@@ -21,8 +21,8 @@ class RSG(core.components.Component):
             for file in self.abstract_task_desc['files']:
                 ext = os.path.splitext(file)[1]
                 if ext == '.c':
-                    models.append(file)
-                    self.logger.debug('Get additional model "{0}'.format(file))
+                    models[file] = {}
+                    self.logger.debug('Get additional model with C file "{0}'.format(file))
                 elif ext == '.aspect':
                     aspects.append(file)
                     self.logger.debug('Get additional aspect "{0}'.format(file))
@@ -64,10 +64,38 @@ class RSG(core.components.Component):
         self.logger.info('Add models to abstract task description')
 
         # Get common and rule specific models.
-        for model in (self.conf.get('common models') or []) + (self.conf.get('models') or []):
-            model = core.utils.find_file_or_dir(self.logger, self.conf['main working directory'], model)
-            self.logger.debug('Get model "{0}"'.format(model))
-            models.append(os.path.relpath(model, os.path.realpath(self.conf['source tree root'])))
+        if 'common models' in self.conf and 'models' in self.conf:
+            for common_model_c_file in self.conf['common models'].keys():
+                if common_model_c_file in self.conf['models'].keys():
+                    raise KeyError('C file "{0}" is specified in both common and rule specific models')
+        if 'models' in self.conf:
+            for model_c_file in self.conf['models'].keys():
+                # Specify additional settings for generated models that have not any settings.
+                if model_c_file.startswith('$'):
+                    for generated_model_c_file in models.keys():
+                        if generated_model_c_file.endswith(model_c_file[1:]):
+                            models[generated_model_c_file] = self.conf['models'][model_c_file]
+                        else:
+                            raise KeyError('Model C file "{0}" was not generated'.format(model_c_file[1:]))
+            # Like common models processed below.
+            for model_c_file in self.conf['models'].keys():
+                if not model_c_file.startswith('$'):
+                    model_c_file_realpath = core.utils.find_file_or_dir(self.logger,
+                                                                        self.conf['main working directory'],
+                                                                        model_c_file)
+                    self.logger.debug('Get model with C file "{0}"'.format(model_c_file_realpath))
+                    models[os.path.relpath(model_c_file_realpath,
+                                           os.path.realpath(self.conf['source tree root']))] = \
+                        self.conf['models'][model_c_file]
+        if 'common models' in self.conf:
+            for common_model_c_file in self.conf['common models'].keys():
+                common_model_c_file_realpath = core.utils.find_file_or_dir(self.logger,
+                                                                           self.conf['main working directory'],
+                                                                           common_model_c_file)
+                self.logger.debug('Get common model with C file "{0}"'.format(common_model_c_file_realpath))
+                models[os.path.relpath(common_model_c_file_realpath,
+                                       os.path.realpath(self.conf['source tree root']))] = \
+                    self.conf['common models'][common_model_c_file]
 
         if not models:
             self.logger.warning('No models are specified')
@@ -81,8 +109,8 @@ class RSG(core.components.Component):
         # Generate CC extra full description file per each model and add it to abstract task description.
         for grp in self.abstract_task_desc['grps']:
             self.logger.info('Add models to group "{0}"'.format(grp['id']))
-            for model in models:
-                out_file = os.path.join('models', '{}.c'.format(os.path.splitext(os.path.basename(model))[0]))
+            for model_c_file in models.keys():
+                out_file = os.path.join('models', '{}.c'.format(os.path.splitext(os.path.basename(model_c_file))[0]))
                 full_desc_file = '{0}.json'.format(out_file)
                 if os.path.isfile(full_desc_file):
                     raise FileExistsError('CC extra full description file "{0}" already exists'.format(full_desc_file))
@@ -91,12 +119,15 @@ class RSG(core.components.Component):
                     json.dump({
                         # Input file path should be relative to source tree root since compilation options are relative
                         # to this directory and we will change directory to that one before invoking preprocessor.
-                        "in files": [model],
+                        "in files": [model_c_file],
                         # Otput file should be located somewhere inside RSG working directory to avoid races.
                         "out file": os.path.relpath(out_file, os.path.realpath(self.conf['source tree root'])),
                         "opts":
                             [string.Template(opt).substitute(hdr_arch=self.conf['sys']['hdr arch']) for opt in
-                             self.conf['model CC options']]
+                             self.conf['model CC options']] +
+                            ['-DLDV_SETS_MODEL_' + (models[model_c_file]['sets model']
+                                                    if 'sets model' in models[model_c_file]
+                                                    else self.conf['common sets model']).upper()]
                     }, fp, sort_keys=True, indent=4)
 
                 grp['cc extra full desc files'].append({
