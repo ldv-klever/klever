bb57bce5c885ea670383c1c8c918e041cab69adc
Ilja Zakharov <ilja.zakharov@ispras.ru>
2016-03-29 16:01:58 +0300

    Replace EMG specifications, EMG options, SA template in job presets

--- bridge/jobs/presets/linux-3.14/rcv.h
+++ bridge/jobs/presets/linux-3.14/rcv.h
@@ -2,69 +2,62 @@
 #define _LDV_RCV_H_
 
 /* If expr evaluates to zero, ldv_assert() causes a program to reach the error
-   label like the standard assert(). */
-#define ldv_assert(expr) ((expr) ? 0 : ldv_error())
+ * function call like the standard assert().
+ */
+#define ldv_assert(expr) ((expr) ? 0 : __VERIFIER_error())
 
-/* The error label wrapper. It is used because of some static verifiers (like
-   BLAST) don't accept multiple error labels through a program. */
-static inline void ldv_error(void)
-{
-  LDV_ERROR: goto LDV_ERROR;
-}
+/* http://sv-comp.sosy-lab.org/2015/rules.php */
+void __VERIFIER_error(void);
 
-/* If expr evaluates to zero, ldv_assume() causes an infinite loop that is
-   avoided by verifiers. */
-#define ldv_assume(expr) ((expr) ? 0 : ldv_stop())
+#define ldv_assume(expr) __VERIFIER_assume(expr)
 
-/* Check that error has not been found until there */
+/* http://sv-comp.sosy-lab.org/2015/rules.php */
+void __VERIFIER_assume(int expr);
+
+/* Check that error has not been found until there. */
 void ldv_check_final_state(void);
-
-/* Infinite loop, that causes verifiers to skip such paths. */
-static inline void ldv_stop(void) {
-  LDV_STOP: goto LDV_STOP;
-}
 
 /* Special nondeterministic functions. */
 int ldv_undef_int(void);
-void *ldv_undef_ptr(void);
+
+
 unsigned long ldv_undef_ulong(void);
 /* Return nondeterministic negative integer number. */
 static inline int ldv_undef_int_negative(void)
 {
-  int ret = ldv_undef_int();
-
-  ldv_assume(ret < 0);
-
-  return ret;
+	int ret = ldv_undef_int();
+	ldv_assume(ret < 0);
+	return ret;
 }
 /* Return nondeterministic nonpositive integer number. */
 static inline int ldv_undef_int_nonpositive(void)
 {
-  int ret = ldv_undef_int();
-
-  ldv_assume(ret <= 0);
-
-  return ret;
+	int ret = ldv_undef_int();
+	ldv_assume(ret <= 0);
+	return ret;
 }
 
-/* Add explicit model for __builin_expect GCC function. Without the model a
-   return value will be treated as nondetermined by verifiers. */
+/* Explicit model for GCC function __builin_expect(). Without this model
+ * return value of __builtin_expect() will be treated as nondetermined by
+ * verifiers.
+ */
 long __builtin_expect(long exp, long c)
 {
-  return exp;
+	return exp;
 }
 
 /* This function causes the program to exit abnormally. GCC implements this
-function by using a target-dependent mechanism (such as intentionally executing
-an illegal instruction) or by calling abort. The mechanism used may vary from
-release to release so you should not rely on any particular implementation.
-http://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html */
+ * function by using a target-dependent mechanism (such as intentionally
+ * executing an illegal instruction) or by calling abort. The mechanism used
+ * may vary from release to release so you should not rely on any particular
+ * implementation (http://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html).
+ */
 void __builtin_trap(void)
 {
-  ldv_assert(0);
+	ldv_assert(0);
 }
 
-/* The constant is for simulating an error of ldv_undef_ptr() function. */
+/* Pointers greater then this number correspond to errors. */
 #define LDV_PTR_MAX 2012
 
 #endif /* _LDV_RCV_H_ */
