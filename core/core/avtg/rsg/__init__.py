a949bc3782501a6c794ba785f3bb50d14d9ad0d4
alexey <alexey@perches.intra.ispras.ru>
2016-04-06 17:31:25 +0300

    Multimodule - rsg supports several groups

--- core/core/avtg/rsg/__init__.py
+++ core/core/avtg/rsg/__init__.py
@@ -76,32 +76,33 @@ class RSG(core.components.Component):
         # CC extra full description files will be put to this directory as well as corresponding output files.
         os.makedirs('models')
 
-        # TODO: at the moment it is assumed that there is the only group in each verification object. Actually we need
-        # to create a separate group and make all other to depend on it.
         # Generate CC extra full description file per each model and add it to abstract task description.
-        for grp in self.abstract_task_desc['grps']:
-            self.logger.info('Add models to group "{0}"'.format(grp['id']))
-            for model in models:
-                out_file = os.path.join('models', '{}.c'.format(os.path.splitext(os.path.basename(model))[0]))
-                full_desc_file = '{0}.json'.format(out_file)
-                if os.path.isfile(full_desc_file):
-                    raise FileExistsError('CC extra full description file "{0}" already exists'.format(full_desc_file))
-                self.logger.debug('Dump CC extra full description to file "{0}"'.format(full_desc_file))
-                with open(full_desc_file, 'w', encoding='ascii') as fp:
-                    json.dump({
-                        # Input file path should be relative to source tree root since compilation options are relative
-                        # to this directory and we will change directory to that one before invoking preprocessor.
-                        "in files": [model],
-                        # Otput file should be located somewhere inside RSG working directory to avoid races.
-                        "out file": os.path.relpath(out_file, os.path.realpath(self.conf['source tree root'])),
-                        "opts":
-                            [string.Template(opt).substitute(hdr_arch=self.conf['sys']['hdr arch']) for opt in
-                             self.conf['model CC options']]
-                    }, fp, sort_keys=True, indent=4)
+        model_grp = {'id': 'models', 'cc extra full desc files': []}
+        for model in models:
+            out_file = os.path.join('models', '{}.c'.format(os.path.splitext(os.path.basename(model))[0]))
+            full_desc_file = '{0}.json'.format(out_file)
+            if os.path.isfile(full_desc_file):
+                raise FileExistsError('CC extra full description file "{0}" already exists'.format(full_desc_file))
+            self.logger.debug('Dump CC extra full description to file "{0}"'.format(full_desc_file))
+            with open(full_desc_file, 'w', encoding='ascii') as fp:
+                json.dump({
+                    # Input file path should be relative to source tree root since compilation options are relative
+                    # to this directory and we will change directory to that one before invoking preprocessor.
+                    "in files": [model],
+                    # Otput file should be located somewhere inside RSG working directory to avoid races.
+                    "out file": os.path.relpath(out_file, os.path.realpath(self.conf['source tree root'])),
+                    "opts":
+                        [string.Template(opt).substitute(hdr_arch=self.conf['sys']['hdr arch']) for opt in
+                         self.conf['model CC options']]
+                }, fp, sort_keys=True, indent=4)
 
-                grp['cc extra full desc files'].append({
-                    'cc full desc file': os.path.relpath(full_desc_file,
-                                                         os.path.realpath(self.conf['source tree root'])),
-                    'rule spec id': self.conf['rule spec id'],
-                    'bug kinds': self.conf['bug kinds']
-                })
+            model_grp['cc extra full desc files'].append({
+                'cc full desc file': os.path.relpath(full_desc_file,
+                                                     os.path.realpath(self.conf['source tree root'])),
+                'rule spec id': self.conf['rule spec id'],
+                'bug kinds': self.conf['bug kinds']
+            })
+        self.abstract_task_desc['grps'].append(model_grp)
+        for dep in self.abstract_task_desc['deps'].values():
+            dep.append(model_grp['id'])
+
