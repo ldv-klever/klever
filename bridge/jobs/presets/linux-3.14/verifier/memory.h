70e61a43e7e8ad2f61e74e05457c6d672da00bbf
Evgeny Novikov <novikov@ispras.ru>
2016-05-05 18:48:31 +0300

    Include proper header

--- bridge/jobs/presets/linux-3.14/verifier/memory.h
+++ bridge/jobs/presets/linux-3.14/verifier/memory.h
@@ -1,6 +1,7 @@
 #ifndef __VERIFIER_MEMORY_H
 #define __VERIFIER_MEMORY_H
-#include <linux/kernel.h>
+
+#include <linux/types.h>
 
 /*ISO/IEC 9899:1999 specification. p. 313, paragraph 7.20.3 "Memory management functions"*/
 extern void *ldv_malloc(size_t size);
