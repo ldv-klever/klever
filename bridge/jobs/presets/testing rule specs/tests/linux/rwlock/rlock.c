dbdd280653b470ca7ac98fd981ac71729d802447
Vitaly Mordan <mordan@ispras.ru>
2016-05-17 13:13:05 +0300

    Added tests for linux:rwlock

---
+++ bridge/jobs/presets/testing rule specs/tests/linux/rwlock/rlock.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/spinlock.h>
+
+static int __init init(void)
+{
+    rwlock_t *rwlock_1;
+    rwlock_t *rwlock_2;
+    rwlock_t *rwlock_3;
+    rwlock_t *rwlock_4;
+
+    unsigned long flags;
+
+    read_lock(rwlock_1);
+
+	return 0;
+}
+
+module_init(init);
