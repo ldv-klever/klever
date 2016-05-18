1e242ac85426363cce09d40e7a6a717e22cf4a81
Vitaly Mordan <mordan@ispras.ru>
2016-05-18 19:45:36 +0300

    Added tests for linux:spinlock:as

---
+++ bridge/jobs/presets/testing rule specs/tests/linux/spinlock/atomic.c
@@ -0,0 +1,19 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/spinlock.h>
+#include <linux/atomic.h>
+
+static int __init my_init(void)
+{
+    spinlock_t *lock_1;
+    spinlock_t *lock_2;
+
+    atomic_t *atomic;
+    atomic_dec_and_lock(atomic, lock_1);
+    spin_unlock(lock_1);
+
+
+	return 0;
+}
+
+module_init(my_init);
---
+++ bridge/jobs/presets/testing rule specs/tests/linux/spinlock/is-locked.c
@@ -0,0 +1,20 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/spinlock.h>
+#include <linux/atomic.h>
+
+static int __init my_init(void)
+{
+    spinlock_t *lock_1;
+    spinlock_t *lock_2;
+
+    spin_trylock(lock_1);
+    if (spin_is_locked(lock_1))
+    {
+        spin_unlock(lock_1);
+    }
+
+	return 0;
+}
+
+module_init(my_init);
