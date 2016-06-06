bdc9ef705769bb95a9dc482b91d9c1bb183e1415
alexey <alexey@perches.intra.ispras.ru>
2016-06-06 17:36:44 +0300

    Multimodule - add separate_modules

---
+++ core/core/lkvog/strategies/separate_modules.py
@@ -0,0 +1,10 @@
+from core.lkvog.strategies.module import Module
+from core.lkvog.strategies.module import Graph
+
+class Separate_modules:
+    def __init__(self, logger, module_deps, params={}):
+        self.logger = logger
+        self.checked_clusters = set()
+
+    def divide(self, module_name):
+        return [Graph([Module(module_name)])]
