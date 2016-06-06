34cc647cdfec86c31f779f69830d7f0b2dcbfc65
Ilja Zakharov <ilja.zakharov@ispras.ru>
2016-06-06 16:56:30 +0300

    Refactor net_device_ops tests

--- bridge/jobs/presets/testing environment model specs/tests/net_device_ops/net_device_callbacks.c
+++ bridge/jobs/presets/testing environment model specs/tests/net_device_ops/net_device_callbacks.c
@@ -1,96 +1,58 @@
-#include <linux/kernel.h>
-#include <linux/mm.h>
 #include <linux/module.h>
-#include <linux/gfp.h>
-#include <linux/usb.h>
-#include <linux/usb/cdc.h>
 #include <linux/netdevice.h>
-#include <linux/if_arp.h>
-#include <linux/if_phonet.h>
-#include <linux/phonet.h>
+#include <linux/emg/test_model.h>
+#include <verifier/nondet.h>
 
-struct net_device *dev;
+struct net_device dev;
 struct mutex *ldv_envgen;
-static int ldv_function(void);
-int deg_lock;
+int flip_a_coin;
 
-struct ldvdriver {
-	void (*handler)(void);
-};
-
-/*
- * Network device callbacks
- */
-static netdev_tx_t ldv_xmit(struct sk_buff *skb, struct net_device *dev){
-	if(deg_lock){
-		mutex_lock(ldv_envgen);
-	}
+static netdev_tx_t ldv_xmit(struct sk_buff *skb, struct net_device *dev)
+{
+    ldv_invoke_middle_callback();
+    return 0;
 }
 
 static int ldv_open(struct net_device *dev)
 {
-	if(deg_lock){
-		mutex_lock(ldv_envgen);
-	}
-	int err;
-	err = ldv_function();
-	if(err){
-		return err;
-	}
-	return 0;
+	int res;
+
+    ldv_invoke_callback();
+    res = ldv_undef_int();
+    if (!res)
+        ldv_probe_up();
+    return res;
 }
 
 static int ldv_close(struct net_device *dev)
 {
-	if(deg_lock){
-		mutex_lock(ldv_envgen);
-	}
-}
-
-static int ldv_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd){
-	if(deg_lock){
-		mutex_lock(ldv_envgen);
-	}
-}
-
-static int ldv_set_mtu(struct net_device *dev, int new_mtu){
-	if(deg_lock){
-		mutex_lock(ldv_envgen);
-	}
+	ldv_release_down();
+    ldv_invoke_callback();
 }
 
 static const struct net_device_ops ldv_ops = {
 	.ndo_open	= ldv_open,
 	.ndo_stop	= ldv_close,
 	.ndo_start_xmit = ldv_xmit,
-	.ndo_do_ioctl	= ldv_ioctl,
-	.ndo_change_mtu = ldv_set_mtu,
-};
-
-static void handler(void)
-{
-	free_netdev(dev);
-	deg_lock = 1;
-};
-
-static struct ldvdriver driver = {
-	.handler =	handler
 };
 
 static int __init ldv_init(void)
 {
-	deg_lock = 0;
-	int err;
-	err = register_netdev(dev);
-	if (err) {
-		return err;
-	}
-	return 0;
+	flip_a_coin = ldv_undef_int();
+    if (flip_a_coin) {
+        dev.netdev_ops = &ldv_ops;
+        ldv_register();
+        return register_netdev(&dev);
+    }
+    return 0;
 }
 
 static void __exit ldv_exit(void)
 {
-	//nothing
+	if (flip_a_coin) {
+        unregister_netdev(&dev);
+        ldv_deregister();
+    }
 }
 
 module_init(ldv_init);
--- bridge/jobs/presets/testing environment model specs/tests/net_device_ops/net_device_invoke.c
+++ bridge/jobs/presets/testing environment model specs/tests/net_device_ops/net_device_invoke.c
@@ -1,71 +1,44 @@
-#include <linux/kernel.h>
-#include <linux/mm.h>
 #include <linux/module.h>
-#include <linux/gfp.h>
-#include <linux/usb.h>
-#include <linux/usb/cdc.h>
 #include <linux/netdevice.h>
-#include <linux/if_arp.h>
-#include <linux/if_phonet.h>
-#include <linux/phonet.h>
+#include <linux/emg/test_model.h>
+#include <verifier/nondet.h>
 
-struct net_device *dev;
+struct net_device dev;
 struct mutex *ldv_envgen;
-static int ldv_function(void);
-int deg_lock;
 
-/*
- * Network device callbacks
- */
-static netdev_tx_t ldv_xmit(struct sk_buff *skb, struct net_device *dev);
+static netdev_tx_t ldv_xmit(struct sk_buff *skb, struct net_device *dev)
+{
+    ldv_invoke_reached();
+    return 0;
+}
 
 static int ldv_open(struct net_device *dev)
 {
-	int err;
-	err = ldv_function();
-	if(err){
-		return err;
-	}
-	mutex_lock(ldv_envgen);
-	deg_lock++;
-	return 0;
+	ldv_invoke_reached();
+    return 0;
 }
 
 static int ldv_close(struct net_device *dev)
 {
-	mutex_lock(ldv_envgen);
-	deg_lock--;
+	ldv_invoke_reached();
+    return 0;
 }
-
-static int ldv_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd);
-
-static int ldv_set_mtu(struct net_device *dev, int new_mtu);
 
 static const struct net_device_ops ldv_ops = {
 	.ndo_open	= ldv_open,
 	.ndo_stop	= ldv_close,
 	.ndo_start_xmit = ldv_xmit,
-	.ndo_do_ioctl	= ldv_ioctl,
-	.ndo_change_mtu = ldv_set_mtu,
 };
 
 static int __init ldv_init(void)
 {
-	deg_lock = 0;
-	int err;
-	err = register_netdev(dev);
-	if (err) {
-		return err;
-	}
-	return 0;
+	dev.netdev_ops = &ldv_ops;
+    return register_netdev(&dev);
 }
 
 static void __exit ldv_exit(void)
 {
-	unregister_netdev(dev);
-	if(deg_lock==1){
-		mutex_unlock(ldv_envgen);
-	}
+	unregister_netdev(&dev);
 }
 
 module_init(ldv_init);
