b3640f76eaac3c0fffcd22ef76468c73aaa1e585
alexey <alexey@perches.intra.ispras.ru>
2016-07-12 16:33:43 +0300

    Removed stuff from testing multimodule strategies

--- bridge/jobs/presets/testing multimodule strategies/tests/linux/external3/big_module.c
+++
@@ -1,8 +0,0 @@
-#include <linux/module.h>
-
-extern void export_err(void);
-int __init big_init(void) {
-    
-    return 0;
-}
-module_init(big_init)
--- bridge/jobs/presets/testing multimodule strategies/tests/linux/external4/subsystem1/ex1.c
+++
@@ -1,7 +0,0 @@
-#include <linux/module.h>
-
-int __init ex2_init(void) {
-  return 0;
-}
-
-module_init(ex2_init);
