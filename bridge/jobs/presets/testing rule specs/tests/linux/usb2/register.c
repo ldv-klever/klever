fe0ad59dffe7554effa954acf3a0873284962d4c
Evgeny Novikov <novikov@ispras.ru>
2016-07-21 17:09:48 +0300

    Simplify and clarify test cases considerably

--- bridge/jobs/presets/testing rule specs/tests/linux/usb2/register.c
+++ bridge/jobs/presets/testing rule specs/tests/linux/usb2/register.c
@@ -1,46 +1,27 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/major.h>
 #include <linux/usb.h>
-#include <linux/netdevice.h>
 
 
-static int usb_ldv_dummy_probe(struct usb_interface *interface,
-                               const struct usb_device_id *id);
-
-static void usb_ldv_dummy_disconnect(struct usb_interface *interface);
-
-static const struct usb_device_id id_table[] = {
-	{ } /* Terminating entry */
-};
-
-static const struct usb_driver ldv_dummy_driver = {
-	.name =       "probe_retval_check",
-	.probe =      usb_ldv_dummy_probe,
-	.disconnect = usb_ldv_dummy_disconnect,
-	.id_table =   id_table,
-};
-
-static int __init init(void)
+static int ldv_usb_probe(struct usb_interface *interface,
+                         const struct usb_device_id *id)
 {
-	return usb_register(&ldv_dummy_driver);
-}
+	int err;
+	struct usb_driver *ldv_usb_driver2;
 
-/* This function are defined here just to make Driver Environment Generator
- * produce their calls. So corresponding test case functions are called too.
- */
-static int usb_ldv_dummy_probe(struct usb_interface *interface,
-                               const struct usb_device_id *id)
-{
-	struct usb_driver *dummy_driver;
+	err = usb_register(ldv_usb_driver2);
 
-	int res = usb_register(dummy_driver);
 	return 0;
 
 }
 
-static void usb_ldv_dummy_disconnect(struct usb_interface *interface)
+static const struct usb_driver ldv_usb_driver = {
+	.probe = ldv_usb_probe
+};
+
+static int __init init(void)
 {
+	return usb_register(&ldv_usb_driver);
 }
 
 module_init(init);
--- bridge/jobs/presets/testing rule specs/tests/linux/usb2/safe.c
+++ bridge/jobs/presets/testing rule specs/tests/linux/usb2/safe.c
@@ -1,54 +1,36 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/major.h>
 #include <linux/usb.h>
 #include <linux/netdevice.h>
 
 
-static int usb_ldv_dummy_probe(struct usb_interface *interface,
-                               const struct usb_device_id *id);
-
-static void usb_ldv_dummy_disconnect(struct usb_interface *interface);
-
-static const struct usb_device_id id_table[] = {
-	{ } /* Terminating entry */
-};
-
-static const struct usb_driver ldv_dummy_driver = {
-	.name =       "probe_retval_check",
-	.probe =      usb_ldv_dummy_probe,
-	.disconnect = usb_ldv_dummy_disconnect,
-	.id_table =   id_table,
-};
-
-static int __init init(void)
+static int ldv_usb_probe(struct usb_interface *interface,
+                         const struct usb_device_id *id)
 {
-	return usb_register(&ldv_dummy_driver);
-}
+	int err;
+	struct usb_driver *ldv_usb_driver2;
+	struct net_device *ldv_net_device;
 
-/* This function are defined here just to make Driver Environment Generator
- * produce their calls. So corresponding test case functions are called too.
- */
-static int usb_ldv_dummy_probe(struct usb_interface *interface,
-                               const struct usb_device_id *id)
-{
-	struct usb_driver *dummy_driver;
-	struct net_device *dummy_net_device;
-
-	int ret = register_netdev(dummy_net_device);
-	if (ret < 0) {
-		return ret;
-	}
-
-	if (usb_register(dummy_driver) < 0) {
+	if (usb_register(ldv_usb_driver2) < 0) {
 		return -1;
 	}
+
+	err = register_netdev(ldv_net_device);
+	if (err < 0) {
+		return err;
+	}
+
 	return 0;
 
 }
 
-static void usb_ldv_dummy_disconnect(struct usb_interface *interface)
+static const struct usb_driver ldv_usb_driver = {
+	.probe = ldv_usb_probe
+};
+
+static int __init init(void)
 {
+	return usb_register(&ldv_usb_driver);
 }
 
 module_init(init);
