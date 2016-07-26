851948b2cbfbbca64a3c7ad9e8538589f76d0dfc
Vitaly Mordan <mordan@ispras.ru>
2016-07-26 19:44:09 +0300

    Added more exceptions

--- core/core/avtg/rsg/__init__.py
+++ core/core/avtg/rsg/__init__.py
@@ -114,7 +114,8 @@ class RSG(core.avtg.plugins.Plugin):
                     fp_out.write('# 1 "{0}"\n'.format(os.path.abspath(model_c_file)))
                     for line in fp_in:
                         fp_out.write(re.sub(r'LDV_(?!PTR)', rule_spec_prefix.upper(),
-                                            re.sub(r'ldv_(?!assert|assume|undef|set|map|in_interrupt_context|is_err|exclusive)',
+                                            re.sub(r'ldv_(?!assert|assume|undef|set|map|in_interrupt_context|is_err|'
+                                                   r'exclusive|zalloc|malloc)',
                                                    rule_spec_prefix, line)))
                 model['prefix preprocessed C file'] = preprocessed_model_c_file
                 self.logger.debug(
