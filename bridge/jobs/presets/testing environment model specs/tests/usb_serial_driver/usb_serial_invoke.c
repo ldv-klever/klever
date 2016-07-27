41a57d4fe21797bc7a66e184007f19672029ce6e
Ilja Zakharov <ilja.zakharov@ispras.ru>
2016-07-27 11:43:06 +0300

    Implement base tests for usb serial interface category

---
+++ bridge/jobs/presets/testing environment model specs/tests/usb_serial_driver/usb_serial_invoke.c
@@ -0,0 +1,41 @@
+#include <linux/module.h>
+#include <linux/tty.h>
+#include <linux/usb.h>
+#include <linux/usb/serial.h>
+#include <linux/emg/test_model.h>
+#include <verifier/nondet.h>
+
+const struct usb_device_id *id_table;
+
+int ldv_probe(struct usb_serial *serial, const struct usb_device_id *id)
+{
+    ldv_invoke_reached();
+    return 0;
+}
+
+void ldv_release(struct usb_serial *serial)
+{
+    ldv_invoke_reached();
+}
+
+static struct usb_serial_driver ldv_driver = {
+    .probe = ldv_probe,
+    .release = ldv_release,
+};
+
+static struct usb_serial_driver * const ldv_drivers[] = {
+         &ldv_driver, NULL
+};
+
+static int __init ldv_init(void)
+{
+    return usb_serial_register_drivers(ldv_drivers, "ldv_driver", id_table);
+}
+
+static void __exit ldv_exit(void)
+{
+     usb_serial_deregister_drivers(ldv_drivers);
+}
+
+module_init(ldv_init);
+module_exit(ldv_exit);
