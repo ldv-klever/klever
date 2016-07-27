f7983217933fde21ed67674364411a0069962097
Evgeny Novikov <novikov@ispras.ru>
2016-07-27 18:16:05 +0300

    Allocate memory of the specified size rather than 1 byte

--- bridge/jobs/presets/linux-3.14/linux/kzalloc.c
+++ bridge/jobs/presets/linux-3.14/linux/kzalloc.c
@@ -4,13 +4,13 @@
 extern void ldv_check_alloc_flags(gfp_t flags);
 extern void ldv_after_alloc(void *res);
 
-void *ldv_kzalloc(gfp_t flags)
+void *ldv_kzalloc(size_t size, gfp_t flags)
 {
 	void *res;
 
 	ldv_check_alloc_flags(flags);
-	res = ldv_verifier_zalloc(1);
+	res = ldv_verifier_zalloc(size);
 	ldv_after_alloc(res);
-	
+
 	return res;
 }
