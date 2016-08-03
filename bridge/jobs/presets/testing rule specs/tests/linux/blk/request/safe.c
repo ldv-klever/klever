d63e229652946c8d2bac69988b5eb46bbaf7b61c
Vitaly Mordan <mordan@ispras.ru>
2016-08-03 14:47:30 +0300

    Fixed some test

--- bridge/jobs/presets/testing rule specs/tests/linux/blk/request/safe.c
+++ bridge/jobs/presets/testing rule specs/tests/linux/blk/request/safe.c
@@ -9,15 +9,16 @@ int __init my_init(void)
 	int x;
 	gfp_t flags = __GFP_WAIT;
 	struct request_queue *queue;
+	struct request *request_1, *request_2;
 
-	struct request *request_1 = blk_get_request(r, x, flags);
+	request_1 = blk_get_request(r, x, flags);
 	if (request_1)
 		blk_put_request(request_1);
 	else
 		return -1;
-//	struct request *request_2 = blk_make_request(r, bio, flags);
-//	if (!IS_ERR(request_2))
-//		__blk_put_request(queue, request_2);
+	request_2 = blk_make_request(r, bio, flags);
+	if (!IS_ERR(request_2))
+		__blk_put_request(queue, request_2);
 	return 0;
 }
 
