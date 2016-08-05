6ac981cd0cfcf10d651d4a5798307813a0b76f6c
Vitaly Mordan <mordan@ispras.ru>
2016-08-05 18:10:14 +0300

    Added linux:clk rule

---
+++ bridge/jobs/presets/testing rule specs/tests/linux/clk/unsafe1.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/clk.h>
+
+static int __init init(void)
+{
+	struct clk *clk_1;
+	struct clk *clk_2;
+	if (!clk_enable(clk_1))
+	{
+		if (clk_enable(clk_2))
+			clk_disable(clk_2);
+		clk_disable(clk_1);
+	}
+	return 0;
+}
+
+module_init(init);
