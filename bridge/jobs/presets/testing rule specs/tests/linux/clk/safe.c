f69ee5db25b27535db024d6a7a902bafd61125f1
Evgeny Novikov <novikov@ispras.ru>
2016-08-05 18:18:17 +0300

    Fix coding style

--- bridge/jobs/presets/testing rule specs/tests/linux/clk/safe.c
+++ bridge/jobs/presets/testing rule specs/tests/linux/clk/safe.c
@@ -6,8 +6,7 @@ static int __init init(void)
 {
 	struct clk *clk_1;
 	struct clk *clk_2;
-	if (!clk_enable(clk_1))
-	{
+	if (!clk_enable(clk_1)) {
 		if (!clk_enable(clk_2))
 			clk_disable(clk_2);
 		clk_disable(clk_1);
