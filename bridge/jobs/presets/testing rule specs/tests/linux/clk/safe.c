d09d47b6b0b299887f2da033950bac343fbfea21
Vitaly Mordan <mordan@ispras.ru>
2016-08-10 13:59:51 +0300

    Improved clk rule (old 320&322)

--- bridge/jobs/presets/testing rule specs/tests/linux/clk/safe.c
+++ bridge/jobs/presets/testing rule specs/tests/linux/clk/safe.c
@@ -6,6 +6,8 @@ static int __init init(void)
 {
 	struct clk *clk_1;
 	struct clk *clk_2;
+	if (IS_ERR_OR_NULL(clk_1) || IS_ERR_OR_NULL(clk_2))
+		return -1;
 	if (!clk_enable(clk_1)) {
 		if (!clk_enable(clk_2))
 			clk_disable(clk_2);
