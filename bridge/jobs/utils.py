cb858a8455d4cb3052f20d60076324cd13c7e06c
vladimir <gratinskiy@ispras.ru>
2016-10-18 13:39:22 +0300

    Don't allow to download files of lightweight jobs.

--- bridge/jobs/utils.py
+++ bridge/jobs/utils.py
@@ -181,7 +181,7 @@ class JobAccess(object):
         return self.job.status == JOB_STATUS[3][0] and (self.__is_author or self.__is_manager) and not self.job.light
 
     def can_dfc(self):
-        return self.job is not None and self.job.status in [JOB_STATUS[3][0], JOB_STATUS[4][0]]
+        return self.job is not None and self.job.status in [JOB_STATUS[3][0], JOB_STATUS[4][0]] and not self.job.light
 
     def __get_prop(self, user):
         if self.job is not None:
