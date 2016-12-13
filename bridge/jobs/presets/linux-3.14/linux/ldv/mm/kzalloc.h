9f0c3f9412110859e0988cc67e449d0d459d6ab7
Ilja Zakharov <ilja.zakharov@ispras.ru>
2016-12-13 16:07:24 +0300

    Revert refactoring of rule specifications sonce it is incomplete and should be done separately

--- bridge/jobs/presets/linux-3.14/linux/ldv/mm/kzalloc.h
+++
@@ -1,6 +0,0 @@
-#include <linux/types.h>
-
-extern void *ldv_kzalloc(size_t size, gfp_t flags);
-
-extern void ldv_check_alloc_flags(gfp_t flags);
-extern void ldv_after_alloc(void *res);
--- bridge/jobs/presets/linux-3.14/linux/mm/kzalloc.c
+++ bridge/jobs/presets/linux-3.14/linux/mm/kzalloc.c
@@ -15,8 +15,11 @@
  * limitations under the License.
  */
 
-#include <linux/ldv/mm/kzalloc.h>
+#include <linux/types.h>
 #include <verifier/memory.h>
+
+extern void ldv_check_alloc_flags(gfp_t flags);
+extern void ldv_after_alloc(void *res);
 
 void *ldv_kzalloc(size_t size, gfp_t flags)
 {
