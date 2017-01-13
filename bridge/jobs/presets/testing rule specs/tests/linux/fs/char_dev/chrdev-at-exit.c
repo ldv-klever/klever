31de44bfd1a5a6a48a540aeb6cca71723ca765a0
Evgeny Novikov <novikov@ispras.ru>
2017-01-13 15:19:29 +0300

    Update CPAchecker to be used by default

--- bridge/jobs/presets/testing rule specs/tests/linux/fs/char_dev/chrdev-at-exit.c
+++ bridge/jobs/presets/testing rule specs/tests/linux/fs/char_dev/chrdev-at-exit.c
@@ -22,9 +22,16 @@
 static int __init init(void)
 {
 	dev_t *dev;
-	unsigned int baseminor, count;
+	unsigned int baseminor, count = 0;
 
-	alloc_chrdev_region(dev, baseminor, count, "test__");
+	switch (alloc_chrdev_region(dev, baseminor, count, "test__")) {
+	    case 1:
+	        count += 1;
+	    case 2:
+	        count += 2;
+	    case 0:
+	        count = 0;
+	}
 
 	return 0;
 }
