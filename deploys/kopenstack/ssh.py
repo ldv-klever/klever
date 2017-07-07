702f78c311b2c227faadb4b5630010f1f7761640
Evgeny Novikov <novikov@ispras.ru>
2017-07-07 18:23:40 +0300

    Fix logging usage

--- deploys/kopenstack/ssh.py
+++ deploys/kopenstack/ssh.py
@@ -56,7 +56,7 @@ class SSH:
                 return self
             except:
                 attempts -= 1
-                logging.exception(
+                self.logger.exception(
                     'Could not establish SSH connection, wait for {0} seconds and try {1} times more'
                     .format(self.CONNECTION_RECOVERY_INTERVAL, attempts))
                 time.sleep(self.CONNECTION_RECOVERY_INTERVAL)
