2508b35828d9da1f93c6443108f3a860a7d73b67
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2017-09-21 14:17:00 +0300

    Update conversion function description.

--- bridge/marks/ConvertTrace.py
+++ bridge/marks/ConvertTrace.py
@@ -69,13 +69,16 @@ are marked with the "note" or "warn" attribute. Returns list of forests.
 
     def thread_call_forests(self):
         """
-This function is extracting the error trace call stack forests.
-The forest is a couple of call trees in the same thread
-if it doesn't have callback actions at all.
-Otherwise the forest is a couple of call trees under callback action.
-Call tree is tree of function names in their execution order.
+This function is extracting the error trace call forests.
+Each forest is one or more call trees in the same thread
+if it does not have callback actions at all.
+Otherwise the forest is a couple of call trees under callback actions.
+A call tree is a tree of function names in their execution order.
 All its leaves are names of functions which calls or statements
-are marked with the "note" or "warn" attribute. Return list of forests.
+are marked with the “note” or “warn” attribute. If there are several
+such functions in a call stack then the latests functions are chosen.
+The function returns a list of forests. A forests order corresponds to
+an execution order of first statements of forest threads.
         """
 
         return ErrorTraceForests(self.error_trace, all_threads=True).trace
