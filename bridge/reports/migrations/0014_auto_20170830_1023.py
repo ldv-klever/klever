093a8e54e6255b926fdf6b3c357e5b16ff6279ae
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2017-09-26 14:30:55 +0300

    Change migration dependency to be the same format as other migrations.

--- bridge/reports/migrations/0014_auto_20170830_1023.py
+++ bridge/reports/migrations/0014_auto_20170830_1023.py
@@ -7,9 +7,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('reports', '0013_clear_comparison_cache.py'),
-    ]
+    dependencies = [('reports', '0013_clear_comparison_cache')]
 
     operations = [
         migrations.AlterField(
