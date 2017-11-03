4d6c089752c3ac76bf1048f44e84241686765fba
Evgeny Novikov <novikov@ispras.ru>
2017-11-03 17:08:29 +0300

    Move LDV_PTR_MAX to the proper place

--- bridge/jobs/presets/linux-3.14/verifier/memory.h
+++ bridge/jobs/presets/linux-3.14/verifier/memory.h
@@ -20,12 +20,6 @@
 
 #include <linux/types.h>
 
-/* Pointers greater then this number correspond to errors. We can't use
- * original value defined in linux/err.h (~(unsigned long)-4095) since it is
- * too hard for verifiers.
- */
-#define LDV_PTR_MAX ((unsigned int)-1)
-
 extern void *ldv_malloc(size_t size);
 extern void *ldv_calloc(size_t nmemb, size_t size);
 extern void *ldv_zalloc(size_t size);
