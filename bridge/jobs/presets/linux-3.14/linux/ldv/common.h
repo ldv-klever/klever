561284fe9eefc4a39c74f8e3b97e13bf83d52e8f
Evgeny Novikov <novikov@ispras.ru>
2017-11-03 16:52:04 +0300

    Move IRQ models from linux/ldv/irq.* to linux/ldv/common.*
    Although they are required rather rarely, they are very simple. From the other side, other rarely used models from linux/ldv/common.* should be moved to separate model files.

--- bridge/jobs/presets/linux-3.14/linux/ldv/common.h
+++ bridge/jobs/presets/linux-3.14/linux/ldv/common.h
@@ -18,6 +18,37 @@
 #ifndef __LINUX_LDV_H
 #define __LINUX_LDV_H
 
+#include <linux/types.h>
+
+/**
+ * ldv_switch_to_interrupt_context() - switch to interrupt context.
+ *
+ * ldv_switch_to_interrupt_context() can be defined by rule specification
+ * models.
+ *
+ * ldv_switch_to_interrupt_context() should be always called by generated
+ * environment models just before calling interrupt callbacks.
+ */
+extern void ldv_switch_to_interrupt_context(void);
+/**
+ * ldv_switch_to_process_context() - switch to process context.
+ *
+ * ldv_switch_to_process_context() can be defined by rule specification
+ * models.
+ *
+ * ldv_switch_to_process_context() should be always called by generated
+ * environment models just after calling interrupt callbacks.
+ */
+extern void ldv_switch_to_process_context(void);
+/**
+ * ldv_in_interrupt_context() - is execution in interrupt context.
+ *
+ * ldv_in_interrupt_context() can be defined by rule specification models.
+ *
+ * Return: True in case of execution in interrupt context and false otherwise.
+ */
+extern bool ldv_in_interrupt_context(void);
+
 /**
  * ldv_initialize() - explicitly initialize rule specification model states.
  *
