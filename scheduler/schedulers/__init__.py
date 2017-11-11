ldv-klever/klever

commit 53c2876b8e22ae7f864a0895f720ab2ab4a68bd4

Do not send useless flush requests

scheduler/schedulers/__init__.py
@@ -460,7 +460,8 @@ class SchedulerExchange(metaclass=abc.ABCMeta):
 
                     # Flushing tasks
                     logging.debug("Flush submitted tasks and jobs if necessary")
-                    self.flush()
+                    if len(tasks_to_start.keys()) > 0:
+                        self.flush()
                 else:
                     logging.warning("Do not run any tasks until actual information about the nodes will be obtained")
 

commit 5cb34392b1421d9b7afccf4f212e171d03b849f1

Fix bug introduced in 53c2876 for Native Scheduler

scheduler/schedulers/__init__.py
@@ -460,7 +460,9 @@ class SchedulerExchange(metaclass=abc.ABCMeta):
 
                     # Flushing tasks
                     logging.debug("Flush submitted tasks and jobs if necessary")
-                    if len(tasks_to_start.keys()) > 0:
+                    if isinstance(tasks_to_start, dict) and len(tasks_to_start.keys()) > 0:
+                        self.flush()
+                    else:
                         self.flush()
                 else:
                     logging.warning("Do not run any tasks until actual information about the nodes will be obtained")