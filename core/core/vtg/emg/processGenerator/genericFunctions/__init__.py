9c2d47afb876ef1fcea39a59b1a805e8863513df
Ilja Zakharov <ilja.zakharov@ispras.ru>
2018-04-13 21:23:43 +0300

    Implement a new translator to call target functions randomly

--- core/core/vtg/emg/processGenerator/genericFunctions/__init__.py
+++ core/core/vtg/emg/processGenerator/genericFunctions/__init__.py
@@ -14,12 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import collections
+import json
 
-from core.vtg.emg.common import model_comment, get_necessary_conf_property, get_conf_property
-from core.vtg.emg.common.c import Function
-from core.vtg.emg.common.c.types import import_declaration
-from core.vtg.emg.common.process import Process, Condition
+from core.utils import find_file_or_dir
+from core.vtg.emg.common import get_necessary_conf_property
+from core.vtg.emg.common.c import Function, Variable
+from core.vtg.emg.common.c.types import Pointer
+from core.vtg.emg.common.process import Process
 
 
 def generate_processes(emg, source, processes, conf):
@@ -33,6 +34,7 @@ def generate_processes(emg, source, processes, conf):
     :param conf: Configuration dictionary of this generator.
     :return: None
     """
+    functions_collection = dict()
 
     # Import Specifications
     emg.logger.info("Generate an entry process on base of given funcitons list")
@@ -42,8 +44,12 @@ def generate_processes(emg, source, processes, conf):
 
     # Read configuration in abstract task
     emg.logger.info("Determine functions")
-    functions_list = emg.abstract_task_desc["functions"]
-    functions_collection = dict()
+    functions_file = emg.abstract_task_desc["functions"]
+    functions_file = find_file_or_dir(emg.logger, emg.conf["main working directory"], functions_file)
+    with open(functions_file, encoding="utf8") as fh:
+        functions_list = json.loads(fh.read())
+    if not isinstance(functions_list, list):
+        raise ValueError("Expect strictly list of functions in file {!r}".format(functions_file))
 
     # Check that all function are valid
     for func in functions_list:
@@ -54,18 +60,19 @@ def generate_processes(emg, source, processes, conf):
             functions_collection[func] = obj
 
     # Read configuration in private configuration about headers
-    headers_map = get_conf_property(conf, "additional headers")
-    if headers_map:
-        for func in (f for f in set(headers_map.keys).intersection(set(functions_list))):
-            functions_collection[func].headers.extend(headers_map[func])
+    # todo: in current implementation it is useless but may help in future
+    # headers_map = get_conf_property(conf, "additional headers")
+    # if headers_map:
+    #     for func in (f for f in set(headers_map.keys).intersection(set(functions_list))):
+    #         functions_collection[func].headers.extend(headers_map[func])
 
     # Genrate scenario
     emg.logger.info('Generate main scenario')
-    new = __generate_calls(emg.logger, conf, functions_collection)
+    new = __generate_calls(emg.logger, emg, conf, functions_collection)
     processes.entry = new
 
 
-def __generate_calls(logger, conf, functions_collection):
+def __generate_calls(logger, emg, conf, functions_collection):
 
     def indented_line(t, s):
         return (t * "\t") + s
@@ -83,7 +90,7 @@ def __generate_calls(logger, conf, functions_collection):
     expressions = []
     for func in functions_collection:
         logger.info("Call function {!r}".format(func))
-        expr = __generate_action(ep, func, functions_collection[func])
+        expr = __generate_call(emg, ep, func, functions_collection[func])
         expressions.append(expr)
 
     # Generate process description
@@ -91,17 +98,24 @@ def __generate_calls(logger, conf, functions_collection):
     tab = 0
     if loop:
         code.append(indented_line(tab, "while (true) {"))
+        tab += 1
 
-    code.append(indented_line(tab, "switch (ldv_nondet_int()) {"))
+    code.append(indented_line(tab, "switch (ldv_undef_int()) {"))
     tab += 1
     cnt = 0
     for expr in expressions:
         code.append(indented_line(tab, "case {}: {}".format(cnt, expr)))
         cnt += 1
+    if loop:
+        code.append(indented_line(tab, "default: break;"))
+    else:
+        code.append(indented_line(tab, "default: ldv_assume(0);"))
+
     tab -= 1
     code.append(indented_line(tab, "}"))
     if loop:
         code.append("}")
+        tab -= 1
 
     ep.add_condition('function_calls', [], code, 'Call all functions independently.')
     ep.process = "<function_calls>"
@@ -109,154 +123,53 @@ def __generate_calls(logger, conf, functions_collection):
     return ep
 
 
-def __generate_action(ep, func, obj):
+def __generate_call(emg, ep, func, obj):
     # Add declaration of caller
     caller_func = Function("ldv_emg_{}_caller".format(func), "void a(void)")
-    ep.add_declaration("environment model", caller_func.name, caller_func.declare(True))
+    ep.add_declaration("environment model", caller_func.name, caller_func.declare(True)[0])
+    expression = ""
+    body = []
+    initializations = []
 
-    # todo: Add definition of caller 
+    # Check retval and cast to void call
+    if obj.declaration.return_value and obj.declaration.return_value.identifier != 'void':
+        expression += "(void) "
 
-
-
-
-
-def __generate_insmod_process(logger, conf, source, inits, exits, kernel_initializations):
-    logger.info("Generate artificial process description to call Init and Exit module functions 'insmod'")
-    ep = Process("insmod")
-    ep.category = 'linux'
-    ep.comment = "Initialize or exit module."
-    ep.self_parallelism = False
-    ep.identifier = 0
-    ep.process = ''
-    ep.pretty_id = 'linux/initialization'
-
-    if len(kernel_initializations) > 0:
-        body = [
-            "int ret;"
-        ]
-        label_name = 'ldv_kernel_initialization_exit'
-
-        # Generate kernel initializations
-        for name, calls in kernel_initializations:
-            for filename, func_name in calls:
-                func = source.get_source_function(func_name, filename)
-                if func:
-                    retval = False if func.declaration.return_value.identifier == 'void' else True
+    # Get arguments and allocate memory for them
+    args = []
+    free_args = []
+    for index, arg in enumerate(obj.declaration.parameters):
+        if not isinstance(arg, str):
+            argvar = Variable("ldv_arg_{}".format(index), arg)
+            body.append(argvar.declare() + ";")
+            args.append(argvar.name)
+            if isinstance(arg, Pointer):
+                # todo: this is an ugly workaround to get translation options directly
+                if get_necessary_conf_property(emg.conf["translation options"], "allocate external"):
+                    value = "external_allocated_data();"
                 else:
-                    raise RuntimeError("Cannot resolve function {!r} in file {!r}".format(name, filename))
-                new_name = __generate_alias(ep, func_name, filename, retval)
-                statements = [
-                    model_comment('callback', func_name, {'call': "{}();".format(func_name)}),
-                ]
-                if retval:
-                    statements.extend([
-                        "ret = {}();".format(new_name),
-                        "ret = ldv_post_init(ret);",
-                        "if (ret)",
-                        "\tgoto {};".format(label_name)
-                    ])
-                else:
-                    statements.append("{}();".format(new_name))
+                    if get_necessary_conf_property(emg.conf["translation options"], "allocate with sizeof"):
+                        value = "ldv_xmalloc(sizeof({}));".\
+                            format(arg.points.to_string('', typedef='complex_and_params'))
+                    else:
+                        value = "ldv_xmalloc_unknown_size(0);"
+                    free_args.append(argvar.name)
+                initializations.append("{} = {}".format(argvar.name, value))
 
-                body.extend(statements)
-        body.extend([
-            "{}:".format(label_name),
-            "return ret;"
-        ])
-        func = Function('ldv_kernel_init', 'int ldv_kernel_init(void)')
-        func.body = body
-        addon = func.define()
-        ep.add_definition('environment model', 'ldv_kernel_init', addon)
-        ki_subprocess = ep.add_condition('kernel_initialization', [], ["%ret% = ldv_kernel_init();"],
-                                         'Kernel initialization stage.')
-        ki_success = ep.add_condition('kerninit_success', ["%ret% == 0"], [], "Kernel initialization is successful.")
-        ki_failed = ep.add_condition('kerninit_failed', ["%ret% != 0"], [], "Kernel initialization is unsuccessful.")
-    if len(inits) > 0:
-        # Generate init subprocess
-        for filename, init_name in inits:
-            new_name = __generate_alias(ep, init_name, filename, True)
-            init_subprocess = Condition(init_name)
-            init_subprocess.comment = 'Initialize the module after insmod with {!r} function.'.format(init_name)
-            init_subprocess.statements = [
-                model_comment('callback', init_name, {'call': "{}();".format(init_name)}),
-                "%ret% = {}();".format(new_name),
-                "%ret% = ldv_post_init(%ret%);"
-            ]
-            logger.debug("Found init function {}".format(init_name))
-            ep.actions[init_subprocess.name] = init_subprocess
+    # Generate call
+    expression += "{}({});".format(func, ", ".join(args))
 
-    # Add ret label
-    ep.add_label('ret', import_declaration("int label"))
+    # Generate function body
+    body += initializations + [expression]
 
-    # Generate exit subprocess
-    if len(exits) == 0:
-        logger.debug("There is no exit function found")
-    else:
-        for filename, exit_name in exits:
-            new_name = __generate_alias(ep, exit_name, filename, False)
-            exit_subprocess = Condition(exit_name)
-            exit_subprocess.comment = 'Exit the module before its unloading with {!r} function.'.format(exit_name)
-            exit_subprocess.statements = [
-                model_comment('callback', exit_name, {'call': "{}();".format(exit_name)}),
-                "{}();".format(new_name)
-            ]
-            logger.debug("Found exit function {}".format(exit_name))
-            ep.actions[exit_subprocess.name] = exit_subprocess
+    # Free memory
+    for arg in free_args:
+        body.append("ldv_free({});".format(arg))
 
-    # Generate conditions
-    success = ep.add_condition('init_success', ["%ret% == 0"], [], "Module has been initialized.")
-    ep.actions[success.name] = success
-    # Generate else branch
-    failed = ep.add_condition('init_failed', ["%ret% != 0"], [], "Failed to initialize the module.")
-    ep.actions[failed.name] = failed
+    caller_func.body = body
 
-    # Add subprocesses finally
-    process = ''
-    for i, pair in enumerate(inits):
-        process += "<{0}>.(<init_failed>".format(pair[1])
-        for j, pair2 in enumerate(exits[::-1]):
-            if pair2[0] == pair[0]:
-                break
-        j = 1
-        for _, exit_name in exits[:j - 1:-1]:
-            process += ".<{}>".format(exit_name)
-        process += "|<init_success>."
+    # Add definition of caller
+    ep.add_definition(obj.definition_file, caller_func.name, caller_func.define() + ["\n"])
 
-    for _, exit_name in exits:
-        process += "<{}>.".format(exit_name)
-
-    if get_conf_property(conf, "check final state"):
-        final_statments = ["ldv_check_final_state();"]
-    else:
-        final_statments = []
-    final_statments += ["ldv_assume(0);"]
-    final = ep.add_condition('final', [], final_statments, "Check rule model state at the exit if required.")
-    process += '<{}>'.format(final.name)
-    process += ")" * len(inits)
-
-    if len(kernel_initializations) > 0 and len(inits) > 0:
-        ep.process += "<{}>.(<{}> | <{}>.({}))".format(ki_subprocess.name, ki_failed.name, ki_success.name, process)
-    elif len(kernel_initializations) == 0 and len(inits) > 0:
-        ep.process += process
-    elif len(kernel_initializations) > 0 and len(inits) == 0:
-        ep.process += "<{}>.(<{}> | <{}>)".format(ki_subprocess.name, ki_failed.name, ki_success.name, process) + \
-                      '.<{}>'.format(final.name)
-    else:
-        raise NotImplementedError("There is no both kernel initilization functions and module initialization functions")
-    return ep
-
-
-def __generate_alias(process, name, file, int_retval=False):
-    new_name = "ldv_emg_{}".format(name)
-    code = [
-        "{}();".format("return {}".format(name) if int_retval else name)
-    ]
-    # Add definition
-    func = Function(new_name,
-                    "{}(void)".format("int {}".format(new_name) if int_retval else "void {}".format(new_name)))
-    func.body = code
-    process.add_definition(file, name, func.define())
-    process.add_declaration('environment model', name,
-                            'extern {} {}(void);\n'.format("int" if int_retval else "void", new_name))
-
-    return new_name
+    # Return call expression
+    return "{}();".format(caller_func.name)
