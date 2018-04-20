02708c1cd187aca0922baade6fbe0602dafe42b6
Evgeny Novikov <novikov@ispras.ru>
2018-04-20 16:51:50 +0300

    Do not print "None" if a user doesn't set private key
    Also, use more appropriate exit code if private key file doesn't exist.

--- deploys/kopenstack/kopenstack.py
+++ deploys/kopenstack/kopenstack.py
@@ -661,7 +661,6 @@ class OSInstance:
 
     def _setup_keypair(self):
         private_key_file = self.args.ssh_rsa_private_key_file
-        self.logger.info('Setup OpenStack keypair using specified private key "{}"'.format(private_key_file))
 
         if not private_key_file:
             self.logger.error('Private key is required. Please specify it using --ssh-rsa-private-key-file argument')
@@ -669,7 +668,9 @@ class OSInstance:
 
         if not os.path.exists(private_key_file):
             self.logger.error('Specified private key "{}" does not exist'.format(private_key_file))
-            sys.exit(errno.EINVAL)
+            sys.exit(errno.ENOENT)
+
+        self.logger.info('Setup OpenStack keypair using specified private key "{}"'.format(private_key_file))
 
         private_key = open(private_key_file, 'rb').read()
 
