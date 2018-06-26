3c94d7152efcb011237380d3da3677c6a8bd86f6
Evgeny Novikov <novikov@ispras.ru>
2018-06-26 13:51:55 +0300

    Remove dangling symbolic links for which os.path.exists() returns false

--- deploys/deploys/local/local.py
+++ deploys/deploys/local/local.py
@@ -184,7 +184,7 @@ class Klever:
                 'klever.json'
         ):
             path = os.path.join(self.args.deployment_directory, path)
-            if os.path.exists(path):
+            if os.path.exists(path) or os.path.islink(path):
                 self.logger.info('Remove "{0}"'.format(path))
                 if os.path.islink(path) or os.path.isfile(path):
                     os.remove(path)
