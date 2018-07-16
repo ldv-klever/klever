41b1db1fe0c08766250970432b45aff15d5e7e20
Evgeny Novikov <novikov@ispras.ru>
2018-07-16 16:40:44 +0300

    Disable debuging for production mode
    https://forge.ispras.ru/issues/9107

--- bridge/bridge/production.py
+++ bridge/bridge/production.py
@@ -17,9 +17,7 @@
 
 from bridge.common import *
 
-# TODO: switch value to False when everything will work fine.
-TEMPLATES[0]['OPTIONS']['debug'] = DEBUG = True
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEF_KLEVER_CORE_MODE = 'production'
 UNLOCK_FAILED_REQUESTS = False
