f0d913267b31dbc5492afd8b9889a5dacbb13506
Pavel Andrianov <andrianov@ispras.ru>
2018-08-14 17:07:28 +0300

    Fixes in svcomp rule

---
+++ bridge/jobs/presets/linux-3.14/verifier/pthread_mutex.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014-2016 ISPRAS (http://www.ispras.ru)
+ * Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * ee the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __VERIFIER_PTHREAD_MUTEX_H
+#define __VERIFIER_PTHREAD_MUTEX_H
+
+typedef unsigned long int pthread_t;
+typedef struct __pthread_internal_list
+{
+  struct __pthread_internal_list *__prev;
+  struct __pthread_internal_list *__next;
+} __pthread_list_t;
+typedef union
+{
+  struct __pthread_mutex_s
+  {
+    int __lock;
+    unsigned int __count;
+    int __owner;
+    unsigned int __nusers;
+    int __kind;
+    int __spins;
+    __pthread_list_t __list;
+  } __data;
+  char __size[40];
+  long int __align;
+} pthread_mutex_t;
+
+extern int pthread_mutex_lock(pthread_mutex_t *mutex);
+
+extern int pthread_mutex_trylock(pthread_mutex_t *mutex);
+
+extern int pthread_mutex_unlock(pthread_mutex_t *mutex);
+
+#endif /* __VERIFIER_PTHREAD_MUTEX_H */
