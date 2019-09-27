6b8a142734d5b62f8bed99423261bd2429d9b2cd
Evgeny Novikov <novikov@ispras.ru>
2019-09-27 17:09:00 +0300

    Remove redundant TODO

--- core/core/session.py
+++ core/core/session.py
@@ -67,7 +67,6 @@ class Session:
 
                 if resp.status_code not in (200, 201, 204):
                     if resp.headers['content-type'] == 'application/json':
-                        # TODO: analyze resp.json()
                         self.error = resp.json()
                         raise BridgeError(
                             'Got error "{0}" when send "{1}" request to "{2}"'.format(self.error, method, url)
