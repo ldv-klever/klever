36950bc9d38746a0769eaffbacf7137dec6acf39
Ilja Zakharov <ilja.zakharov@ispras.ru>
2019-10-28 13:42:48 +0300

    Simplify tests and add tests on interface parsing

--- core/core/vtg/emg/common/c/types/test_parser.py
+++ core/core/vtg/emg/common/c/types/test_parser.py
@@ -18,187 +18,202 @@
 from core.vtg.emg.common.c.types import import_declaration
 
 
-class TestCParser:
+def parser_test(method):
+    def new_method(*args, **kwargs):
+        for test in method(*args, **kwargs):
+            obj = import_declaration(test)
+            # Test that it is parsed
+            assert obj
 
-    def _parser_test(method):
-        def new_method(self, *args, **kwargs):
-            for test in method(self, *args, **kwargs):
-                obj = import_declaration(test)
-                # Test that it is parsed
-                assert obj
+            # Test that it is the same
+            new_obj = import_declaration(obj.to_string('name'))
+            # todo: Pretty names are different but I am not sure whether it correct or not
+            assert obj.compare(new_obj)
 
-                # Test that it is the same
-                new_obj = import_declaration(obj.to_string('name'))
-                # todo: Pretty names are different but I am not sure whether it correct or not
-                assert obj.compare(new_obj)
+    return new_method
 
-        return new_method
 
-    @_parser_test
-    def test_var(self):
-        return [
-            'int a',
-            'int a;',
-            'static int a;'
-        ]
+@parser_test
+def test_var():
+    return [
+        'int a',
+        'int a;',
+        'static int a;'
+    ]
 
-    @_parser_test
-    def test_pars(self):
-        return [
-            'int (a)',
-            'int *(*a)',
-            'int *(**a)',
-            'int *(* const a [])',
-            'int *(* const a) []',
-            'int *(* const a []) [*]',
-            'int *(*(a))',
-            'int (*(*(a) [])) []',
-            'int (*(*(*(a) []))) []',
-            'void (**a)'
-        ]
 
-    @_parser_test
-    def test_bit_fields(self):
-        return [
-            'int a:1',
-            'int a:1;',
-            'unsigned char disable_hub_initiated_lpm : 1'
-        ]
+@parser_test
+def test_pars():
+    return [
+        'int (a)',
+        'int *(*a)',
+        'int *(**a)',
+        'int *(* const a [])',
+        'int *(* const a) []',
+        'int *(* const a []) [*]',
+        'int *(*(a))',
+        'int (*(*(a) [])) []',
+        'int (*(*(*(a) []))) []',
+        'void (**a)'
+    ]
 
-    @_parser_test
-    def test_arrays(self):
-        return [
-            'int a[6U]'
-        ]
 
-    @_parser_test
-    def test_tricky_names(self):
-        return [
-            'int int_a'
-        ]
+@parser_test
+def test_bit_fields():
+    return [
+        'int a:1',
+        'int a:1;',
+        'unsigned char disable_hub_initiated_lpm : 1'
+    ]
 
-    @_parser_test
-    def test_complex_types(self):
-        return [
-            'static int a',
-            'static const int a',
-            'static int const a'
-        ]
 
-    @_parser_test
-    def test_pointers(self):
-        return [
-            'int * a',
-            'int ** a',
-            'int * const a',
-            'int * const * a',
-            'int * const ** a',
-            'int ** const ** a'
-        ]
+@parser_test
+def test_arrays():
+    return [
+        'int a[6U]'
+    ]
 
-    @_parser_test
-    def test_tructs(self):
-        return [
-            'struct usb a',
-            'const struct usb a',
-            'const struct usb * a',
-            'struct usb * const a',
-        ]
 
-    @_parser_test
-    def test_nameless_structs(self):
-        return [
-            "struct {   struct file *file;   struct page *page;   struct dir_context *ctx;   long unsigned int page_index;   u64 *dir_cookie;   u64 last_cookie;   loff_t current_index;   decode_dirent_t decode;   long unsigned int timestamp;   long unsigned int gencount;   unsigned int cache_entry_index;   unsigned char plus : 1;   unsigned char eof : 1; } nfs_readdir_descriptor_t",
-            "struct { short unsigned int size; short unsigned int byte_cnt; short unsigned int threshold; } SR9800_BULKIN_SIZE[8U]"
-        ]
+@parser_test
+def test_tricky_names():
+    return [
+        'int int_a'
+    ]
 
-    @_parser_test
-    def test_unions(self):
-        return [
-            'union usb * const a'
-        ]
 
-    @_parser_test
-    def test_nameless_unions(self):
-        return [
-            'union {   void *arg;   struct kparam_string const *str;   struct kparam_array const *arr; }',
-            'union {   s64 lock;    } arch_rwlock_t',
-            'union {   s64 lock;   struct   {     u32 read;     s32 write;   }; } arch_rwlock_t'
-        ]
+@parser_test
+def test_complex_types():
+    return [
+        'static int a',
+        'static const int a',
+        'static int const a'
+    ]
 
-    @_parser_test
-    def test_typedefs(self):
-        return [
-            'mytypedef * a'
-        ]
 
-    @_parser_test
-    def test_matrix(self):
-        return [
-            'int a []',
-            'int a [1]',
-            'int a [const 1]',
-            'int a [*]',
-            'int a [const *]',
-            'int a [const *][1]',
-            'int a [const *][1][]',
-            'static struct usb ** a [const 1][2][*]'
-        ]
+@parser_test
+def test_pointers():
+    return [
+        'int * a',
+        'int ** a',
+        'int * const a',
+        'int * const * a',
+        'int * const ** a',
+        'int ** const ** a'
+    ]
 
-    @_parser_test
-    def test_functions(self):
-        return [
-            'int a(int)',
-            'int a(int, int)',
-            'int a(void)',
-            'void a(void)',
-            'void a(int, ...)',
-            'int func(struct nvme_dev *, void *)',
-            'void func(struct nvme_dev *, void *, struct nvme_completion *)'
-        ]
 
-    @_parser_test
-    def test_function_pointers(self):
-        return [
-            "void (*a) (int, ...)",
-            "int (*f)(int *)",
-            "int (*f)(int *, int *)",
-            "int (*f)(struct nvme_dev *, void *)",
-            "void (**a)(struct nvme_dev *, void *)",
-            "void (**a)(void)",
-            "void (**a)(struct nvme_dev * a)",
-            "void (**a)(struct nvme_dev * a, int)",
-            "void (**a)(struct nvme_dev * a, void * a)",
-            "void (**a)(struct nvme_dev *, void *)",
-            "void (**a)(struct nvme_dev *, void *, struct nvme_completion *)",
-            "void (**a)(struct nvme_dev *, void *, int (*)(void))"
-        ]
+@parser_test
+def test_tructs():
+    return [
+        'struct usb a',
+        'const struct usb a',
+        'const struct usb * a',
+        'struct usb * const a',
+    ]
 
-    @_parser_test
-    def test_function_pointer_args(self):
-        return [
-            "int _prf(int (*func)())",
-            "static int func(int, void (*)(void))",
-            "static int (*func)(int, void (*)(void))",
-            "static int (*func [])(int, void (*)(void))",
-            "int ** a(int **(*(*arg))(void))",
-            "int func(int, void (*)(void))",
-            "int func(void (*)(void), int)",
-            "int func(int, int (*)(int))",
-            "int func(int, void (*)(void *))",
-            "int func(int *, void (*)(void))",
-            "int func(int, int (*)(int))",
-            "int func(int *, int (*)(int, int))",
-            "int func(int *, int (*)(int, int), ...)",
-            "int func(int (*)(int))",
-            "int func(int (*)(int *), ...)",
-            "int func(int (*)(int, ...))",
-            "int func(int (*)(int, ...), ...)",
-            "int (*a)(int (*)(int, ...), ...)"
-        ]
 
-    @_parser_test
-    def test_mess_declarations(self):
-        return [
-            'void (*((*a)(int, ...)) []) (void) []'
-        ]
+@parser_test
+def test_nameless_structs():
+    return [
+        "struct {   struct file *file;   struct page *page;   struct dir_context *ctx;   long unsigned int page_index;   u64 *dir_cookie;   u64 last_cookie;   loff_t current_index;   decode_dirent_t decode;   long unsigned int timestamp;   long unsigned int gencount;   unsigned int cache_entry_index;   unsigned char plus : 1;   unsigned char eof : 1; } nfs_readdir_descriptor_t",
+        "struct { short unsigned int size; short unsigned int byte_cnt; short unsigned int threshold; } SR9800_BULKIN_SIZE[8U]"
+    ]
+
+
+@parser_test
+def test_unions():
+    return [
+        'union usb * const a'
+    ]
+
+
+@parser_test
+def test_nameless_unions():
+    return [
+        'union {   void *arg;   struct kparam_string const *str;   struct kparam_array const *arr; }',
+        'union {   s64 lock;    } arch_rwlock_t',
+        'union {   s64 lock;   struct   {     u32 read;     s32 write;   }; } arch_rwlock_t'
+    ]
+
+
+@parser_test
+def test_typedefs():
+    return [
+        'mytypedef * a'
+    ]
+
+
+@parser_test
+def test_matrix():
+    return [
+        'int a []',
+        'int a [1]',
+        'int a [const 1]',
+        'int a [*]',
+        'int a [const *]',
+        'int a [const *][1]',
+        'int a [const *][1][]',
+        'static struct usb ** a [const 1][2][*]'
+    ]
+
+
+@parser_test
+def test_functions():
+    return [
+        'int a(int)',
+        'int a(int, int)',
+        'int a(void)',
+        'void a(void)',
+        'void a(int, ...)',
+        'int func(struct nvme_dev *, void *)',
+        'void func(struct nvme_dev *, void *, struct nvme_completion *)'
+    ]
+
+
+@parser_test
+def test_function_pointers():
+    return [
+        "void (*a) (int, ...)",
+        "int (*f)(int *)",
+        "int (*f)(int *, int *)",
+        "int (*f)(struct nvme_dev *, void *)",
+        "void (**a)(struct nvme_dev *, void *)",
+        "void (**a)(void)",
+        "void (**a)(struct nvme_dev * a)",
+        "void (**a)(struct nvme_dev * a, int)",
+        "void (**a)(struct nvme_dev * a, void * a)",
+        "void (**a)(struct nvme_dev *, void *)",
+        "void (**a)(struct nvme_dev *, void *, struct nvme_completion *)",
+        "void (**a)(struct nvme_dev *, void *, int (*)(void))"
+    ]
+
+
+@parser_test
+def test_function_pointer_args():
+    return [
+        "int _prf(int (*func)())",
+        "static int func(int, void (*)(void))",
+        "static int (*func)(int, void (*)(void))",
+        "static int (*func [])(int, void (*)(void))",
+        "int ** a(int **(*(*arg))(void))",
+        "int func(int, void (*)(void))",
+        "int func(void (*)(void), int)",
+        "int func(int, int (*)(int))",
+        "int func(int, void (*)(void *))",
+        "int func(int *, void (*)(void))",
+        "int func(int, int (*)(int))",
+        "int func(int *, int (*)(int, int))",
+        "int func(int *, int (*)(int, int), ...)",
+        "int func(int (*)(int))",
+        "int func(int (*)(int *), ...)",
+        "int func(int (*)(int, ...))",
+        "int func(int (*)(int, ...), ...)",
+        "int (*a)(int (*)(int, ...), ...)"
+    ]
+
+
+@parser_test
+def test_mess_declarations():
+    return [
+        'void (*((*a)(int, ...)) []) (void) []'
+    ]
--- core/core/vtg/emg/common/process/test_parser.py
+++ core/core/vtg/emg/common/process/test_parser.py
@@ -18,68 +18,71 @@
 from core.vtg.emg.common.process import parse_process
 
 
-class TestProcessParser:
+def parse_assert(original_method):
+    def test_method(*args, **kwargs):
+        for test in original_method(*args, **kwargs):
+            obj = parse_process(test)
+            assert obj
 
-    def parse_assert(original_method):
-        def test_method(self, *args, **kwargs):
-            for test in original_method(self, *args, **kwargs):
-                obj = parse_process(test)
-                assert obj
-
-        return test_method
-
-    @parse_assert
-    def test_spaces(self):
-        return [
-            "([a].[b]) | [c]",
-            "[a].[b] | [c]",
-            "[a] | [b] | [c]",
-            "(([a].[b] | [c]) . [d]) | [e]"
-        ]
-
-    @parse_assert
-    def test_pars(self):
-        return [
-            "([a[2]])",
-            "([a].[b]).[c]"
-        ]
-
-    @parse_assert
-    def test_operators(self):
-        return [
-            "<free>.(instance_deregister)",
-            "[one].[two]",
-            "[one] | [two]",
-            "<assign>.[register] | <none>",
-            "<assign> | [one].[two]"
-        ]
+    return test_method
 
 
-    @parse_assert
-    def test_multiple_operators(self):
-        return [
-            "<assign>.[open].(ret_open).[register] | <none>",
-        ]
+@parse_assert
+def test_spaces():
+    return [
+        "([a].[b]) | [c]",
+        "[a].[b] | [c]",
+        "[a] | [b] | [c]",
+        "(([a].[b] | [c]) . [d]) | [e]"
+    ]
 
-    @parse_assert
-    def test_indexes(self):
-        return [
-            "(!register).[instance_register[%k%]].[instance_deregister[%k%]].(deregister)"
-        ]
 
-    @parse_assert
-    def test_subprocess(self):
-        return [
+@parse_assert
+def test_pars():
+    return [
+        "([a[2]])",
+        "([a].[b]).[c]"
+    ]
 
-            "(!instance_register).<alloc>.<init>.[probe].(ret_probe).(<probe_success>.((usb_reset).[pre].(ret_pre).[post]."
-            "(ret_post) | [suspend].(ret_suspend).[resume].(ret_resume) | <null>).[release].(ret_release) | <failed_probe>)."
-            "[callback].{call} | <positive_probe>.[release].<after_release>.{call} |  <positive_probe>.[suspend].(ret_suspend)."
-            "(<suspended>.[resume].(ret_resume) | <not_suspended>).{call} | <negative_probe>.(<free>.(deregister) | [probe]."
-            "(ret_probe).{call})"
-        ]
 
-    @parse_assert
-    def test_broadband_send(self):
-        return [
-            "[@usb_reset]"
-        ]
+@parse_assert
+def test_operators():
+    return [
+        "<free>.(instance_deregister)",
+        "[one].[two]",
+        "[one] | [two]",
+        "<assign>.[register] | <none>",
+        "<assign> | [one].[two]"
+    ]
+
+
+@parse_assert
+def test_multiple_operators():
+    return [
+        "<assign>.[open].(ret_open).[register] | <none>",
+    ]
+
+
+@parse_assert
+def test_indexes():
+    return [
+        "(!register).[instance_register[%k%]].[instance_deregister[%k%]].(deregister)"
+    ]
+
+
+@parse_assert
+def test_subprocess():
+    return [
+        "(!instance_register).<alloc>.<init>.[probe].(ret_probe).(<probe_success>.((usb_reset).[pre].(ret_pre).[post]."
+        "(ret_post) | [suspend].(ret_suspend).[resume].(ret_resume) | <null>).[release].(ret_release)|<failed_probe>)."
+        "[callback].{call}|<positive_probe>.[release].<after_release>.{call}| <positive_probe>.[suspend].(ret_suspend)."
+        "(<suspended>.[resume].(ret_resume)|<not_suspended>).{call}|<negative_probe>.(<free>.(deregister)|[probe]."
+        "(ret_probe).{call})"
+    ]
+
+
+@parse_assert
+def test_broadband_send():
+    return [
+        "[@usb_reset]"
+    ]
