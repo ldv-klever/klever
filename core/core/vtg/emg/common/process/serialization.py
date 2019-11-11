62efb53441759ef67cc667d1873a37befd6c77c3
Ilja Zakharov <ilja.zakharov@ispras.ru>
2019-11-11 15:21:54 +0300

    Adjust the class for specification import to the new interface

--- core/core/vtg/emg/common/process/serialization.py
+++ core/core/vtg/emg/common/process/serialization.py
@@ -15,16 +15,12 @@
 # limitations under the License.
 #
 
-import os
 import json
 
 from core.vtg.emg.common.c.types import import_declaration
 from core.vtg.emg.common.process.parser import parse_process
-from core.vtg.emg.common.process import Receive, Dispatch, Subprocess, Block, Label, Process
-
-
-from core.vtg.emg.common.process import Action, Choice, Concatenation, Parentheses, ProcessCollection
-from core.vtg.emg.common.process import Receive, Dispatch, Subprocess, Block
+from core.vtg.emg.common.process import Receive, Dispatch, Subprocess, Block, Label, Process, Action, Choice, \
+    Concatenation, Parentheses, ProcessCollection
 
 
 class CollectionEncoder(json.JSONEncoder):
@@ -122,30 +118,41 @@ class CollectionDecoder:
     """
 
     PROCESS_CONSTRUCTOR = Process
-    LABEL_CONSTRUCTOR = Label
     PROCESS_ATTRIBUTES = {
+        'headers': None,
+        'declarations': None,
+        'definitions': None,
         'source files': 'cfiles'
     }
     ACTION_ATTRIBUTES = {
+        'comment': None,
+        'parameters': None,
+        'condition': None,
+        'statements': None,
+        'peers': None,
         'pre-call': 'pre_call',
         'post-call': 'post_call',
         'entry point': 'trace_relevant'
+    }
+    LABEL_CONSTRUCTOR = Label
+    LABEL_ATTRIBUTES = {
+        'value': None,
+        'declaration': None
     }
 
     def __init__(self, logger, conf):
         self.logger = logger
         self.conf = conf
 
-    def parse_event_specification(self, Source, raw):
+    def parse_event_specification(self, source, raw):
         """
         Parse process descriptions and create corresponding objects to populate the collection.
 
-        :param Source: Source code coollection.
+        :param source: Source code collection.
         :param raw: Dictionary with content of JSON file.
         :return: ProcessCollection
         """
-        env_processes = dict()
-        models = dict()
+        collection = ProcessCollection()
 
         self.logger.info("Import processes from provided event categories specification")
         if "functions models" in raw:
@@ -154,118 +161,50 @@ class CollectionDecoder:
                 names = name_list.split(", ")
                 for name in names:
                     self.logger.debug("Import process which models {!r}".format(name))
-                    models[name] = self._import_process(name, process_desc)
 
                     # Set some default values
-                    if not models[name]._category:
-                        models[name]._category = "functions models"
-                    if models[name]._category != "functions models":
-                        raise ValueError("Each function model specification should has category 'functions models' but "
-                                         "process {!r} has name {!r}".format(str(models[name]), models[name]._category))
-                    models[short_name].pretty_id = "functions models/{}".format(short_name)
+                    category = "functions models"
+                    _, pname = name.split('/')
+                    process = self._import_process(source, pname, category, process_desc)
+                    collection.models[process] = process
         if "environment processes" in raw:
             self.logger.info("Import processes from 'environment processes'")
             for name, process_desc in raw["environment processes"].items():
                 self.logger.debug("Import environment process {!r}".format(name))
 
-                category, pname = get_short_name(name, process_desc)
-                process = self._import_process(pname, process_desc)
-                if not process._category:
-                    process._category = category
-                if pname in env_processes:
-                    raise ValueError("There is already imported process {!r} with identifier {!r} in intermediate "
-                                     "environment model with name {!r} and category {!r} and identifier {!r}".
-                                     format(pname, process.identifier, env_processes[pname]._name,
-                                            env_processes[pname]._category, env_processes[pname].identifier))
-                env_processes[pname] = process
-                process.pretty_id = "{}/{}".format(process._category, process._name)
+                category, pname = name.split('/')
+                process = self._import_process(source, pname, category, process_desc)
+                if process in collection.environment:
+                    raise ValueError("There is an already imported process {!r} in intermediate environment model".
+                                     format(str(process)))
+                collection.environment[process] = process
         if "main process" in raw and isinstance(raw["main process"], dict):
             self.logger.info("Import main process")
-            entry_process = self._import_process("entry", raw["main process"])
-            if not entry_process._category:
-                entry_process._category = 'entry process'
-            if not entry_process.pretty_id:
-                entry_process.pretty_id = "{}/entry".format(entry_process._category)
+            entry_process = self._import_process(source, "entry", "entry process", raw["main process"])
+            collection.entry = entry_process
         else:
-            entry_process = None
+            collection.entry = None
 
-        self.models = models
-        self.environment = env_processes
-        self.entry = entry_process
+        return collection
 
+    def _import_process(self, source, name, category, dic):
+        process = self.PROCESS_CONSTRUCTOR(name, category)
 
-
-    def establish_peers(self, strict=False):
-        """
-        Get processes and guarantee that all peers are correctly set for both receivers and dispatchers. The function
-        replaces dispatches expressed by strings to object references as it is expected in translation.
-
-        :param strict: Raise exception if a peer process identifier is unknown (True) or just ignore it (False).
-        :return: None
-        """
-        # Then check peers. This is because in generated processes there no peers set for manually written processes
-        processes = list(self.models.values()) + list(self.environment.values()) + ([self.entry] if self.entry else [])
-        process_map = {p.pretty_id: p for p in processes}
-        for process in processes:
-            for action in [process.actions[a] for a in process.actions
-                           if isinstance(process.actions[a], Receive) or isinstance(process.actions[a], Dispatch) and
-                           len(process.actions[a].peers) > 0]:
-                new_peers = []
-                for peer in action.peers:
-                    if isinstance(peer, str):
-                        if peer in process_map:
-                            target = process_map[peer]
-                            new_peer = {'process': target, 'subprocess': target.actions[action.name]}
-                            new_peers.append(new_peer)
-
-                            opposite_peers = [p['process'].pretty_id if isinstance(p, dict) else p
-                                              for p in target.actions[action.name].peers]
-                            if process.pretty_id not in opposite_peers:
-                                target.actions[action.name].peers.append({'process': process, 'subprocess': action})
-                        elif strict:
-                            raise KeyError("Process {!r} tries to send a signal {!r} to {!r} but there is no such "
-                                           "process in the model".format(process.pretty_id, action.name, peer))
-                    else:
-                        new_peers.append(peer)
-
-                action.peers = new_peers
-
-            # Set names
-            tokens = process.pretty_id.split('/', maxsplit=1)
-            if len(tokens) < 2:
-                raise ValueError('Cannot extract category/name prefix from process identifier {!r}'.
-                                 format(process.pretty_id))
-            else:
-                process.category = tokens[0]
-                process.name = tokens[1]
-
-
-
-    def _import_process(self, name, dic):
-        process = self.PROCESS_CONSTRUCTOR(name)
-
-        def get_abspath(path):
-            if path == 'environment model' or os.path.isfile(path):
-                return path
-            for spath in self.conf["working source trees"]:
-                abspath = os.path.join(spath, path)
-                storabspath = self._clade.get_storage_path(abspath)
-                if os.path.isfile(storabspath):
-                    return abspath
-            else:
-                raise FileNotFoundError('There is no file {!r} in the build base or the correct path to source files'
-                                        ' is not provided'.format(path))
-
-        if 'labels' in dic:
-            for label_name in dic['labels']:
-                label = self._import_label(label_name, dic['labels'][label_name])
-                process.labels[label_name] = label
+        for label_name in dic.get('labels', {}):
+            label = self._import_label(label_name, dic['labels'][label_name])
+            process.labels[label_name] = label
 
         # Import process
         if 'process' in dic:
-            process.process = parse_process(process, dic['process'])
+            parse_process(process, dic['process'])
         else:
             raise KeyError("Each process must have 'process' attribute, but {!r} misses it".format(name))
+
+        # Then import subprocesses
+        for desc in dic.get('actions', {}).values():
+            subp = desc.get('process')
+            if subp:
+                parse_process(process, subp)
 
         # Import comments
         if 'comment' in dic:
@@ -275,14 +214,11 @@ class CollectionDecoder:
                 "You must specify manually 'comment' attribute within the description of {!r} kernel "
                 "function model process".format(name))
 
-        # Import subprocesses
-        if 'actions' in dic:
-            for action_name in dic['actions']:
-                action = self._import_action(process, action_name, dic['actions'][action_name])
-                process.actions[action_name] = action
-
-                if 'process' in dic['actions'][action_name]:
-                    parse_process(process, dic['actions'][action_name]['process'])
+        # Import actiones
+        for action_name in dic.get('actions', {}):
+            if not process.actions.get(action_name):
+                raise ValueError('Action {!r} was not used in {!r} process'.format(action_name, str(process)))
+            self._import_action(process.actions[action_name], dic['actions'][action_name])
 
         for att in self.PROCESS_ATTRIBUTES:
             if att in dic:
@@ -297,44 +233,36 @@ class CollectionDecoder:
             # Avoid iterating over the dictionary that can change its content
             if att in dic:
                 for def_file in dic[att].keys():
-                    dic[att][get_abspath(def_file)] = dic[att].pop(def_file)
+                    dic[att][source.find_file(def_file)] = dic[att].pop(def_file)
                 # Update object to be sure that changes are saved there
                 setattr(process, att, dic[att])
 
-        unused_labels = process.unused_labels
+        unused_labels = {str(l) for l in process.unused_labels}
         if len(unused_labels) > 0:
-            raise RuntimeError("Found unused labels in process {!r}: {}".
-                               format(process._name, ', '.join(unused_labels)))
+            raise RuntimeError("Found unused labels in process {!r}: {}".format(str(process), ', '.join(unused_labels)))
         if process.file != 'entry point':
-            process.file = get_abspath(process.file)
+            process.file = source.find_file(process.file)
 
         process.accesses()
         return process
 
-    def _import_action(self, process, name, dic):
-        act = process.actions.get(name)
-        if not act:
-            raise KeyError('Action %s is not used in process %s' % (name, process))
-
-        for att in self.ACTION_ATTRIBUTES:
-            if att in dic:
-                if self.ACTION_ATTRIBUTES[att]:
-                    attname = self.ACTION_ATTRIBUTES[att]
-                else:
-                    attname = att
-                setattr(act, attname, dic[att])
-        return act
+    def _import_action(self, act, dic):
+        for att in (att for att in self.ACTION_ATTRIBUTES if att in dic):
+            if self.ACTION_ATTRIBUTES[att]:
+                attname = self.ACTION_ATTRIBUTES[att]
+            else:
+                attname = att
+            setattr(act, attname, dic[att])
 
     def _import_label(self, name, dic):
         label = self.LABEL_CONSTRUCTOR(name)
 
-        for att in self.LABEL_ATTRIBUTES:
-            if att in dic:
-                if self.LABEL_ATTRIBUTES[att]:
-                    attname = self.LABEL_ATTRIBUTES[att]
-                else:
-                    attname = att
-                setattr(label, attname, dic[att])
+        for att in (att for att in self.LABEL_ATTRIBUTES if att in dic):
+            if self.LABEL_ATTRIBUTES[att]:
+                attname = self.LABEL_ATTRIBUTES[att]
+            else:
+                attname = att
+            setattr(label, attname, dic[att])
 
         if label.declaration:
             label.declaration = import_declaration(label.declaration)
