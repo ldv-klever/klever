158976cfb912cba62c62bc4502edc2430461255c
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2019-12-03 11:32:50 +0300

    Make users administration better.

--- bridge/users/admin.py
+++ bridge/users/admin.py
@@ -16,6 +16,25 @@
 #
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+
 from users.models import User
 
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
+        (_('Preferences'), {'fields': (
+            'accuracy', 'data_format', 'language', 'timezone',
+            'default_threshold', 'assumptions', 'triangles', 'coverage_data'
+        )}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'role', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
+    filter_horizontal = ()
+
+
+admin.site.register(User, CustomUserAdmin)
