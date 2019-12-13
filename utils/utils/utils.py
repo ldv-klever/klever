359262c29ddc8ce2871a50be8b5518e1df1c7c57
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2019-12-13 13:54:18 +0300

    Fix bug with unknown author on job versions duplicating.

--- utils/utils/utils.py
+++ utils/utils/utils.py
@@ -174,7 +174,7 @@ class Session:
         :param job_uuid: job identifier
         :return: None
         """
-        self.__request('jobs/api/duplicate/{0}/'.format(job_uuid), 'PATCH')
+        self.__request('jobs/api/duplicate/{0}/'.format(job_uuid), 'PUT')
 
     def create_preset_job(self, preset_uuid):
         """
