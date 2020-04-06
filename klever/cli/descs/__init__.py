ldv-klever/klever

commit 7e3e1cea3a4ae4fea4c01ced67b33ac87fa7022d

Improve command-line interface of klever-build
It is possible now to specify the following values:
* path to the directory that contains all required git repositorues
* path to the directory where build bases will be stored
* list of target program descriptions to use

Also:
* Move all target program descriptions to separate json files
* Remove mandatory 'build base' prefix from all descriptions

klever/cli/descs/__init__.py
@@ -1,0 +1,77 @@
+# Copyright (c) 2020 ISP RAS (http://www.ispras.ru)
+# Ivannikov Institute for System Programming of the Russian Academy of Sciences
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+preset_jobs_dir = os.path.normpath(
+    os.path.join(
+        os.path.dirname(__file__), os.path.pardir, os.path.pardir, os.path.pardir,
+        'bridge', 'jobs', 'presets'
+    )
+)
+
+common_target_program_descs = {
+    'Linux': {
+        'source code': 'linux-stable',
+        'git repository version': 'v3.14.79',
+        'configuration': 'allmodconfig',
+        'architecture': 'x86_64',
+        'model CC options file': 'scripts/mod/empty.c',
+        'external modules header files search directory': os.path.join(preset_jobs_dir, 'specifications'),
+        'loadable kernel modules': ['all'],
+        'allow local source trees use': True,
+        'generate makefiles': True,
+        'extra headers': [
+            'linux/user_namespace.h',
+            'linux/tty.h',
+            'linux/tty_driver.h',
+            'linux/usb.h',
+            'linux/usb/serial.h',
+            'linux/platform_device.h',
+            'linux/netdevice.h',
+            'linux/net.h',
+            'linux/timer.h',
+            'linux/interrupt.h',
+            'linux/seq_file.h',
+            'linux/i2c.h',
+            'linux/mod_devicetable.h',
+            'linux/device.h',
+            'linux/pm.h',
+            'linux/fs.h',
+            'linux/rtnetlink.h',
+            'net/mac80211.h',
+            'linux/iio/iio.h',
+            'linux/iio/triggered_buffer.h',
+            'linux/cdev.h',
+            'linux/miscdevice.h',
+            'linux/pci.h',
+            'linux/rtc.h',
+            'scsi/scsi_host.h',
+            'linux/pagemap.h',
+            'linux/poll.h',
+            'linux/blkdev.h',
+            'target/target_core_base.h',
+            'target/target_core_backend.h',
+            'linux/spi/spi.h',
+            'linux/fb.h'
+        ],
+        'extra Clade options': {
+            'Info.extra_CIF_opts': [
+                '-D__GNUC__=4',
+                '-D__GNUC_MINOR__=6'
+            ]
+        }
+    }
+}

klever/cli/descs/unsupported.py
@@ -1,0 +1,94 @@
+# Copyright (c) 2020 ISP RAS (http://www.ispras.ru)
+# Ivannikov Institute for System Programming of the Russian Academy of Sciences
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TODO: Unsupported tests.
+unsupported_descs = [
+    {
+        'build base': 'testing/57d7f5',
+        'name': 'Linux',
+        'git repository version': 'v4.15.18',
+        'external modules': 'linux/testing/environment model specs/tests',
+        'loadable kernel modules': [
+            'ext-modules/block_device_operations_v.1',
+            'ext-modules/class_v.1',
+            'ext-modules/devm_threaded_irq_v.1',
+            'ext-modules/ethtool_ops_v.2',
+            'ext-modules/file_operations_v.1',
+            'ext-modules/hid_v.1',
+            'ext-modules/ieee80211_ops_v.1',
+            'ext-modules/iio_triggered_buffer_v.1',
+            'ext-modules/irq_v.2',
+            'ext-modules/net_device_ops_v.2',
+            'ext-modules/pci_driver_v.1',
+            'ext-modules/percpu_irq_v.1',
+            'ext-modules/platform_driver_v.1',
+            'ext-modules/proto_v.2',
+            'ext-modules/rtc_class_ops_v.1',
+            'ext-modules/scsi_host_template_v.1',
+            'ext-modules/seq_operations_v.1',
+            'ext-modules/serial_core_v.1',
+            'ext-modules/target_backend_v.1',
+            'ext-modules/tty_v.2',
+            'ext-modules/usb_driver_v.1',
+            'ext-modules/usb_serial_driver_v.2',
+            'ext-modules/timer_v.3',
+            'ext-modules/workqueue_v.1',
+            'ext-modules/urb_v.1',
+            'ext-modules/tasklet_v.2',
+            'ext-modules/kthread_v.1'
+        ],
+        'exclude extra headers': ['linux/poll.h'],
+        # Linux 4.15.18 can be built with new versions of GCC.
+        'extra Clade options': {}
+    },
+    {
+        'build base': 'testing/7a7e7e',
+        'name': 'Linux',
+        'git repository version': 'v4.17.19',
+        'external modules': 'linux/testing/environment model specs/tests',
+        'loadable kernel modules': [
+            'ext-modules/block_device_operations_v.1',
+            'ext-modules/class_v.1',
+            'ext-modules/devm_threaded_irq_v.1',
+            'ext-modules/ethtool_ops_v.2',
+            'ext-modules/file_operations_v.1',
+            'ext-modules/hid_v.1',
+            'ext-modules/ieee80211_ops_v.1',
+            'ext-modules/iio_triggered_buffer_v.1',
+            'ext-modules/irq_v.2',
+            'ext-modules/net_device_ops_v.2',
+            'ext-modules/pci_driver_v.1',
+            'ext-modules/percpu_irq_v.1',
+            'ext-modules/platform_driver_v.1',
+            'ext-modules/proto_v.2',
+            'ext-modules/rtc_class_ops_v.1',
+            'ext-modules/scsi_host_template_v.1',
+            'ext-modules/seq_operations_v.1',
+            'ext-modules/serial_core_v.1',
+            'ext-modules/target_backend_v.1',
+            'ext-modules/tty_v.2',
+            'ext-modules/usb_driver_v.1',
+            'ext-modules/usb_serial_driver_v.2',
+            'ext-modules/timer_v.3',
+            'ext-modules/workqueue_v.1',
+            'ext-modules/urb_v.1',
+            'ext-modules/tasklet_v.2',
+            'ext-modules/kthread_v.1'
+        ],
+        'exclude extra headers': ['linux/poll.h'],
+        # Linux v4.17.19 can be built with new versions of GCC.
+        'extra Clade options': {}
+    }
+]

klever/cli/klever_build.py
@@ -15,8 +15,11 @@
 # limitations under the License.
 #
 
-from distutils.dir_util import copy_tree
+import argparse
+import distutils.dir_util
+import glob
 import hashlib
+import json
 import os
 import re
 import shutil
@@ -28,7 +31,7 @@ import urllib.parse
 
 from clade import Clade
 from klever.cli.utils import execute_cmd, get_logger, make_relative_path
-from klever.cli.descs import common_target_program_descs, target_program_descs
+from klever.cli.descs import preset_jobs_dir, common_target_program_descs
 
 
 class CProgram:
@@ -96,9 +99,9 @@ class CProgram:
             else:
                 self.logger.debug("Source code is provided in form of source tree")
 
-            if 'Git repository version' in self.target_program_desc:
+            if 'git repository version' in self.target_program_desc:
                 self.logger.info('Checkout Git repository "{0}"'
-                                 .format(self.target_program_desc['Git repository version']))
+                                 .format(self.target_program_desc['git repository version']))
 
                 # Always remove Git repository lock file .git/index.lock if it exists since it can remain after
                 # some previous Git commands crashed.
@@ -109,7 +112,7 @@ class CProgram:
                 # In case of dirty Git working directory checkout may fail so clean up it first.
                 execute_cmd(self.logger, 'git', 'clean', '-f', '-d', cwd=self.work_src_tree)
                 execute_cmd(self.logger, 'git', 'reset', '--hard', cwd=self.work_src_tree)
-                execute_cmd(self.logger, 'git', 'checkout', '-f', self.target_program_desc['Git repository version'],
+                execute_cmd(self.logger, 'git', 'checkout', '-f', self.target_program_desc['git repository version'],
                             cwd=self.work_src_tree)
 
                 # Use Git describe to properly identify C program version which source code is provided in form of Git
@@ -372,7 +375,8 @@ class Linux(CProgram):
 
         # Linux kernel configuration can be specified by means of configuration file or configuration target.
         if os.path.isfile(self.target_program_desc['configuration']):
-            conf_file = self.target_program_desc['configuration']
+            # preset_jobs_dir is common directory for all configuration files
+            conf_file = os.path.join(preset_jobs_dir, self.target_program_desc['configuration'])
 
             self.logger.info('Linux kernel configuration file is "{0}"'.format(conf_file))
 
@@ -415,6 +419,9 @@ class Linux(CProgram):
         if not ext_modules:
             return None
 
+        # preset_jobs_dir is common directory for all external modules
+        ext_modules = os.path.join(preset_jobs_dir, ext_modules)
+
         tmp_dir = tempfile.mkdtemp()
         self.tmp_dirs.append(tmp_dir)
 
@@ -431,7 +438,7 @@ class Linux(CProgram):
 
         if os.path.isdir(ext_modules):
             self.logger.debug('External loadable Linux kernel modules source code is provided in form of source tree')
-            copy_tree(ext_modules, work_src_tree)
+            distutils.dir_util.copy_tree(ext_modules, work_src_tree)
         elif os.path.isfile(ext_modules):
             self.logger.debug('External loadable Linux kernel modules source code is provided in form of archive')
             with tarfile.open(ext_modules, encoding='utf8') as TarFile:
@@ -439,7 +446,7 @@ class Linux(CProgram):
 
         self.logger.info('Make canonical working source tree of external loadable Linux kernel modules')
         work_src_tree_root = None
-        for dirpath, dirnames, filenames in os.walk(work_src_tree):
+        for dirpath, _, filenames in os.walk(work_src_tree):
             ismakefile = False
             for filename in filenames:
                 if filename == 'Makefile':
@@ -487,16 +494,113 @@ class BusyBox(CProgram):
         super().__init__(logger, target_program_desc)
 
 
-def klever_build():
+def get_descs_dir():
+    return os.path.join(os.path.dirname(__file__), 'descs')
+
+
+def get_desc_paths(desc_name_pattern='*'):
+    desc_paths = []
+
+    for desc_path in glob.glob(os.path.join(get_descs_dir(), '{}.json'.format(desc_name_pattern))):
+        desc_paths.append(desc_path)
+
+    return desc_paths
+
+
+def get_all_desc_names():
+    # Get names of all json files with target program descriptions (without .json extension)
+    desc_names = []
+
+    for desc_path in get_desc_paths():
+        file_name = os.path.basename(desc_path)
+        desc_names.append(os.path.splitext(file_name)[0])
+
+    return desc_names
+
+
+def parse_args(logger, sys_args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-o',
+        '--output',
+        help='path to the directory where build bases will be stored. Default: {!r}'.format('build bases'),
+        default='build bases',
+        metavar='PATH'
+    )
+
+    parser.add_argument(
+        '-r',
+        '--repositories',
+        help='path to the directory that contains all required git repositorues (linux-stable, busybox)',
+        default='.',
+        metavar='PATH'
+    )
+
+    parser.add_argument(
+        '-l',
+        '--list',
+        help='show the list of available target program descriptions and exit',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        dest='descriptions',
+        nargs=argparse.REMAINDER,
+        help='list of descriptions to use. Glob patterns are also supported',
+    )
+
+    args = parser.parse_args(sys_args)
+
+    if args.list:
+        logger.info('Available target program descriptions:\n{}'.format(
+            '\n'.join(sorted(get_all_desc_names()))
+        ))
+        sys.exit()
+
+    if not args.descriptions:
+        logger.error('You need to specify at least one target program description')
+        sys.exit(-1)
+
+    return args
+
+
+def klever_build(sys_args=sys.argv[1:]):
     logger = get_logger(__name__)
 
-    for target_program_desc in target_program_descs:
-        logger.info('Prepare build base "{}"'.format(target_program_desc['build base']))
-        common_target_program_desc = dict(common_target_program_descs[target_program_desc['name']])
-        common_target_program_desc.update(target_program_desc)
-        getattr(sys.modules[__name__], target_program_desc['name'])(logger, common_target_program_desc).build()
-        logger.info('Build base "{}" was successfully prepared'.format(target_program_desc['build base']))
+    args = parse_args(logger, sys_args)
+
+    all_desc_paths = []
+
+    for desc_name_pattern in args.descriptions:
+        desc_paths = get_desc_paths(desc_name_pattern)
+        all_desc_paths.extend(desc_paths)
+
+        if not desc_paths:
+            logger.error('There are no json files corresponding to the specified description pattern {!r}'.format(
+                desc_name_pattern
+            ))
+            logger.error('Targer program descriptions are stored in the {!r} directory'.format(get_descs_dir()))
+            sys.exit(-1)
+
+    for desc_path in all_desc_paths:
+        with open(desc_path, 'r', encoding='utf-8') as fp:
+            descs = json.load(fp)
+
+        for desc in descs:
+            desc['build base'] = os.path.abspath(os.path.join(args.output, desc['build base']))
+            logger.info('Prepare build base "{}"'.format(desc['build base']))
+
+            common_desc = dict(common_target_program_descs[desc['name']])
+            common_desc.update(desc)
+            common_desc['source code'] = os.path.abspath(os.path.join(args.repositories, common_desc['source code']))
+
+            CProgramClass = getattr(sys.modules[__name__], desc['name'])
+            CProgramObj = CProgramClass(logger, common_desc)
+            CProgramObj.build()
+
+            logger.info('Build base "{}" was successfully prepared'.format(desc['build base']))
 
 
 if __name__ == '__main__':
-    klever_build()
+    klever_build(sys.argv[1:])

commit 208886bdb8f472cd42f76b7573a4e2076b03ded5

Remove global simulation of old GCC
This was implemented in 37333191f4fa6aedb14b2cbd7f7f0a1ed74695f8 but
sudddenly that was broken in 7e3e1cea3a4ae4fea4c01ced67b33ac87fa7022d.

klever/cli/descs/__init__.py
@@ -66,12 +66,6 @@ common_target_program_descs = {
             'target/target_core_backend.h',
             'linux/spi/spi.h',
             'linux/fb.h'
-        ],
-        'extra Clade options': {
-            'Info.extra_CIF_opts': [
-                '-D__GNUC__=4',
-                '-D__GNUC_MINOR__=6'
-            ]
-        }
+        ]
     }
 }