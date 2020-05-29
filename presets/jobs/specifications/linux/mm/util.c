e66a88309b7ba3f62b0b4d4e27bf6178ea4a69df
Evgeny Novikov <novikov@ispras.ru>
2020-05-29 15:26:37 +0300

    Develop better model for kmemdup
    The previous model missed memory copying and it was incompatible with
    new versions of the Linux kernel.

---
+++ presets/jobs/specifications/linux/mm/util.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 ISP RAS (http://www.ispras.ru)
+ * Ivannikov Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/types.h>
+#include <ldv/linux/string.h>
+#include <ldv/verifier/memory.h>
+
+void *ldv_kmemdup(const void *src, size_t len, gfp_t gfp)
+{
+	void *res;
+
+	ldv_check_alloc_flags(gfp);
+	res = ldv_malloc(len);
+	ldv_after_alloc(res);
+
+	if (res)
+		memcpy(res, src, len);
+  
+	return res;
+}
+
