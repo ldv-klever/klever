2906914c9b93573d28fa4dcc1ad07e13495f4db8
Evgeny Novikov <novikov@ispras.ru>
2020-06-10 19:04:37 +0300

    Add test cases for request/release-firmware

---
+++ klever/cli/descs/linux/testing/common models/tests/linux/drivers/base/firmware_loader/safe.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020 ISP RAS (http://www.ispras.ru)
+ * Ivannikov Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/module.h>
+#include <linux/firmware.h>
+#include <ldv/test.h>
+
+static int __init ldv_init(void)
+{
+	const struct firmware *fw;
+	const char *fw_name;
+	struct device *dev;
+	int err;
+
+	dev = ldv_undef_ptr_non_null();
+	err = request_firmware(&fw, fw_name, dev);
+
+	if (err)
+	{
+		if (fw)
+			ldv_unexpected_error();
+
+		return ldv_undef_int_negative();
+	}
+
+	if (!fw)
+		ldv_unexpected_error();
+
+	if (!fw->data)
+		ldv_unexpected_error();
+
+	release_firmware(fw);
+
+	return 0;
+}
+
+module_init(ldv_init);
