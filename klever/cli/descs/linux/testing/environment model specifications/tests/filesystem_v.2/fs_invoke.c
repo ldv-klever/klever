677d394517d2e1b8b8b26d983c339888ac22cb67
Evgeny Novikov <novikov@ispras.ru>
2020-08-27 19:44:04 +0300

    Add test cases for the new filesystem API

---
+++ klever/cli/descs/linux/testing/environment model specifications/tests/filesystem_v.2/fs_invoke.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018 ISP RAS (http://www.ispras.ru)
+ * Ivannikov Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <ldv/linux/emg/test_model.h>
+#include <ldv/verifier/nondet.h>
+
+static struct dentry *ldv_mount(struct file_system_type *fs_type, int flags, const char *dev_name, void *data)
+{
+	ldv_invoke_reached();
+	return NULL;
+}
+
+static void ldv_kill_sb(struct super_block *sb)
+{
+	ldv_invoke_reached();
+}
+
+static struct file_system_type ldv_fs = {
+	.mount = ldv_mount,
+	.kill_sb = ldv_kill_sb,
+};
+
+static int __init ldv_init(void)
+{
+	ldv_invoke_test();
+	return register_filesystem(&ldv_fs);
+}
+
+static void __exit ldv_exit(void)
+{
+	unregister_filesystem(&ldv_fs);
+}
+
+module_init(ldv_init);
+module_exit(ldv_exit);
---
+++ klever/cli/descs/linux/testing/environment model specifications/tests/filesystem_v.2/unregister_filesystem.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2018 ISP RAS (http://www.ispras.ru)
+ * Ivannikov Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <ldv/linux/emg/test_model.h>
+#include <ldv/verifier/common.h>
+#include <ldv/verifier/nondet.h>
+
+static struct dentry *ldv_mount(struct file_system_type *fs_type, int flags, const char *dev_name, void *data)
+{
+	struct dentry *dentry;
+
+	ldv_invoke_callback();
+	dentry = ldv_undef_ptr();
+	ldv_assume(!IS_ERR(dentry));
+
+	return dentry;
+}
+
+static void ldv_kill_sb(struct super_block *sb)
+{
+	ldv_invoke_callback();
+}
+
+static struct file_system_type ldv_fs = {
+	.mount = ldv_mount,
+	.kill_sb = ldv_kill_sb,
+};
+
+static int __init ldv_init(void)
+{
+	int ret = ldv_undef_int();
+	int flip_a_coin = ldv_undef_int();
+
+	if (flip_a_coin) {
+		ldv_register();
+		ret = register_filesystem(&ldv_fs);
+		if (!ret) {
+			unregister_filesystem(&ldv_fs);
+		}
+		ldv_deregister();
+	}
+
+	return ret;
+}
+
+static void __exit ldv_exit(void)
+{
+	/* pass */
+}
+
+module_init(ldv_init);
+module_exit(ldv_exit);
