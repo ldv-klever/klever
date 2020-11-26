aa34d64deb4a48da84c3b71077d968880dad6ec1
Evgeny Novikov <novikov@ispras.ru>
2020-11-26 19:35:24 +0300

    Update Clade to solve issue with processing empty models

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setuptools.setup(
     install_requires=[
         "Django==3.0.6",
         "BenchExec==1.18",
-        "clade==3.2.14",
+        "clade==3.2.15",
         "psycopg2",
         "graphviz",
         "celery",
