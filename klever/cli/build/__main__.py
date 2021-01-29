89a8be275ead3516fc26fafd3b4075d50bba74c5
Evgeny Novikov <novikov@ispras.ru>
2021-01-29 12:53:59 +0300

    Add CIF, cross CIFs and cross build tools to PATH

--- klever/cli/build/__main__.py
+++ klever/cli/build/__main__.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import argparse
+import configparser
 import importlib
 import json
 import os
@@ -37,7 +38,7 @@ def get_desc_paths(desc_name_pattern=None):
         desc_paths.append(str(desc_path))
 
     if desc_name_pattern:
-        desc_paths = [x for x in desc_paths if desc_name_pattern in os.path.relpath(x, get_descs_dir())]
+        desc_paths = [x for x in desc_paths if desc_name_pattern + '/' in os.path.relpath(x, get_descs_dir())]
 
     return desc_paths
 
@@ -80,6 +81,76 @@ def import_build_modules():
         importlib.import_module(module_name, 'klever.cli.build')
 
 
+def add_cif_and_cross_build_tools_to_path(logger):
+    # klever-build (Clade) requires CIF and cross CIFs. Besides, it has sense to use corresponding cross build tools for
+    # normal build processes. So, add everything required to PATH.
+    # Here we rely on the Klever installation and make some assumptions regarding it as well as regarding arrangement of
+    # CIF binaries. Correspondingly, let's try to mitigate various issues that can happen with this.
+
+    def_warn_msg = 'you should provide necessary CIF, cross CIFs and cross build tools via PATH yourself'
+
+    # Try to get Klever deployment directory.
+    if not os.path.isfile('/etc/default/klever'):
+        logger.warning('Klever deployment settings file "/etc/default/klever" does not exist ({0})'
+                       .format(def_warn_msg))
+        return
+
+    klever_deploy_settings = configparser.ConfigParser()
+    with open('/etc/default/klever') as fp:
+        # Add stub section to look like normal INI file.
+        klever_deploy_settings.read_string('[root]\n' + fp.read())
+
+    if 'KLEVER_DEPLOYMENT_DIRECTORY' not in klever_deploy_settings['root']:
+        logger.warning('Could not determine Klever deployment directory ({0})'.format(def_warn_msg))
+        return
+
+    # Truncate double quotes around the setting value.
+    klever_deploy_dir = klever_deploy_settings['root']['KLEVER_DEPLOYMENT_DIRECTORY'][1:-1]
+    logger.info('Klever deployment directory is "{0}"'.format(klever_deploy_dir))
+
+    klever_deploy_conf_file = os.path.join(klever_deploy_dir, 'klever.json')
+    if not os.path.isfile(klever_deploy_conf_file):
+        logger.warning('Klever deployment configuration file "{0}" does not exist ({1})'
+                       .format(klever_deploy_conf_file, def_warn_msg))
+        return
+
+    with open(klever_deploy_conf_file) as fp:
+        klever_deploy_conf = json.load(fp)
+
+    if 'Klever Addons' not in klever_deploy_conf:
+        logger.warning('Klever deployment configuration file "{0}" does not describe any Klever addon ({1})'
+                       .format(klever_deploy_conf_file, def_warn_msg))
+        return
+
+    klever_addons = klever_deploy_conf['Klever Addons']
+    dirs_to_path = []
+    for name, desc in klever_addons.items():
+        if 'version' in desc:
+            dirs_to_path.append(os.path.join(klever_deploy_dir, 'klever-addons', name,
+                                             desc['executable path'] if 'executable path' in desc else ''))
+
+    if not dirs_to_path:
+        logger.warning('Klever deployment configuration file "{0}" does not describe Klever addons at top level ({1})'
+                       .format(klever_deploy_conf_file, def_warn_msg))
+        return
+
+    # Determine directories of cross build tools to be added to PATH as real directories of Aspectator (GCC) binaries.
+    # Don't consider non-cross build tools since various versions of GCC are necessary to build all testing and
+    # validation build bases (in this case Aspectator binary does not have any prefix - it is exactly "aspectator").
+    cross_build_tool_dirs = []
+    for dir_to_path in dirs_to_path:
+        for dirpath, _, filenames in os.walk(dir_to_path):
+            for filename in filenames:
+                if filename.endswith('-aspectator') and os.path.islink(os.path.join(dirpath, filename)):
+                    cross_build_tool_dirs.append(os.path.dirname(os.path.realpath(os.path.join(dirpath, filename))))
+
+    logger.info('Add following directories to PATH:\n{0}'
+                .format('\n'.join(['  {0}'.format(d) for d in dirs_to_path + cross_build_tool_dirs])))
+
+    for d in dirs_to_path + cross_build_tool_dirs:
+        os.environ["PATH"] = "{}:{}".format(d, os.environ["PATH"])
+
+
 def parse_args(args, logger):
     parser = argparse.ArgumentParser()
 
@@ -94,7 +165,7 @@ def parse_args(args, logger):
     parser.add_argument(
         '-r',
         '--repositories',
-        help='path to the directory that contains all required git repositorues (linux-stable, userspace)',
+        help='path to the directory that contains all required git repositories (linux-stable, userspace)',
         default='.',
         metavar='PATH'
     )
@@ -142,6 +213,7 @@ def main(args=sys.argv[1:]):
             sys.exit(-1)
 
     import_build_modules()
+    add_cif_and_cross_build_tools_to_path(logger)
 
     for desc_path in all_desc_paths:
         with open(desc_path, 'r', encoding='utf-8') as fp:
