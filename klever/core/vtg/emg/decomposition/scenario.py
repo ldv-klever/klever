f3a79272a29c7158a97590a1e83f491430216eac
Ilja Zakharov <ilja.zakharov@ispras.ru>
2021-02-23 19:41:13 +0300

    Update scenarios up to the new actions interface

--- klever/core/vtg/emg/decomposition/scenario.py
+++ klever/core/vtg/emg/decomposition/scenario.py
@@ -17,116 +17,155 @@
 
 import copy
 
-from klever.core.vtg.emg.common.process.actions import Actions, Subprocess, Concatenation, Choice, Parentheses, Operator
+from klever.core.vtg.emg.common.process.actions import Savepoint, BaseAction, Operator, Behaviour, Concatenation, \
+    Choice, Parentheses, Subprocess, Actions
 
 
 class Scenario:
 
-    def __init__(self, savepoint=None):
+    def __init__(self, savepoint: Savepoint = None):
+        assert isinstance(savepoint, Savepoint) or savepoint is None,\
+            f'Receive incorrect object of type {type(savepoint).__name__}'
+
         self.savepoint = savepoint
         self.actions = Actions()
         self.__initial_action = None
 
-    def set_initial_action(self, action):
+    @property
+    def initial_action(self):
+        return self.__initial_action
+
+    @initial_action.setter
+    def initial_action(self, behaviour: Operator):
+        assert isinstance(behaviour, Operator), \
+            f'Expect an operator instead of {type(behaviour).__name__}'
+
         if not self.__initial_action:
-            new = self._add_action_copy(action)
+            new = self._add_action_copy(behaviour)
             self.__initial_action = new
         else:
             raise ValueError(f'An initial action {str(self.__initial_action)} is already set')
 
-    def add_action_copy(self, action, operator):
-        assert operator in self.actions.values()
-
+    def add_action_copy(self, action: BaseAction, operator: Operator = None):
+        assert isinstance(operator, Operator) or operator is None
         new_copy = self._add_action_copy(action)
 
-        if isinstance(operator, Parentheses):
-            assert not operator.action, 'Parent already has a child'
-            operator.action = new_copy
-        elif isinstance(operator, Choice):
-            operator.add_action(new_copy)
-        elif isinstance(operator, Concatenation):
-            operator.add_action(new_copy)
-        else:
-            raise RuntimeError(f'Unknown operator {type(action).__name__} at copying action {str(action)}')
+        if operator is not None:
+            assert operator in self.actions.behaviour()
+            operator.append(new_copy)
 
         return new_copy
 
-    def _add_action_copy(self, action):
-        new_copy = copy.deepcopy(action)
-        self.actions[str(new_copy)] = new_copy
+    def _add_action_copy(self, behaviour: BaseAction):
+        assert isinstance(behaviour, BaseAction), \
+            f'Expect a base action instead of {type(behaviour).__name__}'
+
+        new_copy = behaviour.clone()
         new_copy.my_operator = None
-
-        if isinstance(new_copy, Operator):
-            new_copy.clean()
-
+        if isinstance(behaviour, Operator):
+            new_copy.clear()
+            self.actions.add_process_action(new_copy)
+        elif isinstance(behaviour, Behaviour):
+            self.actions.add_process_action(new_copy, new_copy.name)
+        else:
+            raise NotImplementedError
         return new_copy
 
 
 class ScenarioExtractor:
 
-    def __init__(self, actions):
+    def __init__(self, actions: Actions):
         self._actions = actions
         self._roots = {self._actions.initial_action}
 
-    def __call__(self, model):
-        return self.__create_scenarios()
+    def __call__(self):
+        scenarios = self.__create_scenarios()
+        return scenarios
 
-    def _process_subprocess(self, scenario, action, operator=None):
-        self.__process_operator(scenario, action, operator)
+    def _process_subprocess(self, scenario, beh, operator=None, cache=set()):
+        assert isinstance(beh, Behaviour)
+        assert beh.kind is Subprocess
 
-    def _process_concatenation(self, scenario, action, operator=None):
-        self.__process_operator(scenario, action, operator)
+        new = self._process_leaf_action(scenario, beh, operator)
+        if beh.name not in cache:
+            cache.add(beh.name)
+            child = beh.description.action
+            new_action = self._fill_top_down(scenario, child)
+            assert new_action
+            new.description.action = new_action
+        return new
 
-    def _process_choice(self, scenario, action, operator=None):
-        self.__process_operator(scenario, action, operator)
+    def _process_concatenation(self, scenario, beh, operator=None):
+        return self.__process_operator(scenario, beh, operator)
 
-    def _process_parentheses(self, scenario, action, operator=None):
-        self.__process_operator(scenario, action, operator)
+    def _process_choice(self, scenario, beh, operator=None):
+        return self.__process_operator(scenario, beh, operator)
 
-    def _process_leaf_action(self, scenario, action, operator=None):
-        assert operator
-        scenario.add_action_copy(action, operator)
+    def _process_parentheses(self, scenario, beh, operator=None):
+        return self.__process_operator(scenario, beh, operator)
+
+    def _process_leaf_action(self, scenario, beh, operator=None):
+        assert isinstance(beh, Behaviour)
+
+        cpy = scenario.add_action_copy(beh, operator)
+        if beh.name not in scenario.actions:
+            new_description = copy.copy(self._actions[beh.name])
+            scenario.actions[beh.name] = new_description
+        return cpy
 
     def _get_scenarios_for_root_savepoints(self, root):
-        for savepoint in root.savepoints:
-            new_scenario = Scenario(savepoint)
-            new_scenario.set_initial_action(root)
-            self._fill_top_down(new_scenario, root)
-            yield new_scenario
+        def new_scenario(rt, svp=None):
+            nsc = Scenario(svp)
+            nsc.initial_action = rt
+            for child in rt:
+                self._fill_top_down(nsc, child, nsc.initial_action)
+            return nsc
 
-    def _fill_top_down(self, scenario, action, operator=None):
-        if isinstance(action, Concatenation):
+        first_actual = self._actions.first_actions(root)
+        assert len(first_actual) == 1, 'Support only the one first action'
+        actual = self._actions.behaviour(first_actual.pop())
+        assert len(actual) == 1, f'Support only the one first action behaviour'
+        actual = actual.pop()
+        if actual.description.savepoints:
+            for savepoint in actual.description.savepoints:
+                yield new_scenario(self._actions.initial_action, savepoint)
+        else:
+            yield new_scenario(self._actions.initial_action)
+
+    def _fill_top_down(self, scenario: Scenario, beh: Behaviour, operator: Operator = None):
+        assert isinstance(beh, BaseAction)
+        assert isinstance(operator, Operator) or operator is None
+        assert beh not in scenario.actions.behaviour()
+        if operator:
+            assert operator in scenario.actions.behaviour()
+
+        if isinstance(beh, Concatenation):
             processing_method = self._process_concatenation
-        elif isinstance(action, Choice):
+        elif isinstance(beh, Choice):
             processing_method = self._process_choice
-        elif isinstance(action, Parentheses):
+        elif isinstance(beh, Parentheses):
             processing_method = self._process_parentheses
-        elif isinstance(action, Subprocess):
+        elif isinstance(beh, Behaviour) and beh.kind is Subprocess:
             processing_method = self._process_subprocess
         else:
             processing_method = self._process_leaf_action
 
-        processing_method(scenario, action, operator)
+        return processing_method(scenario, beh, operator)
 
     def __create_scenarios(self):
-        scenarios = set()
+        scenarios = list()
         while self._roots:
             root = self._roots.pop()
 
             for new_scenario in self._get_scenarios_for_root_savepoints(root):
-                scenarios.add(new_scenario)
+                scenarios.append(new_scenario)
         return scenarios
 
-    def __process_operator(self, scenario, action, operator=False):
-        if operator:
-            parent = scenario.add_action_copy(action, operator)
-        else:
-            parent = scenario.actions.initial_action
+    def __process_operator(self, scenario, behaviour, operator=None):
+        assert isinstance(behaviour, Operator), type(behaviour).__name__
+        parent = scenario.add_action_copy(behaviour, operator)
 
-        if hasattr(action, 'action'):
-            self._fill_top_down(scenario, action.action, parent)
-        elif hasattr(action, 'actions'):
-            for child in action.actions.values():
-                self._fill_top_down(scenario, child, parent)
-        else:
-            raise ValueError(f'Operator {type(action).__name__} {str(action)} has not childen')
+        for child in behaviour:
+            self._fill_top_down(scenario, child, parent)
+
+        return parent
