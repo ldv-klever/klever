65b6386957af300f0c763218f91196a0040c02ff
Evgeny Novikov <novikov@ispras.ru>
2021-04-03 17:48:02 +0300

    Merge function enter and exit edges
    Implements https://forge.ispras.ru/issues/10749.
    Most code was taken from commit 3346c22 that deleted it. Before that
    commit 5362778 switched off merging as it was not accurate enough. The
    new version tries to solve corresponding issues though let's whether it
    is good enough.

--- klever/core/vrp/et/error_trace.py
+++ klever/core/vrp/et/error_trace.py
@@ -713,6 +713,30 @@ class ErrorTrace:
         if removed_switch_cases_num:
             self._logger.debug('{0} switch cases were removed'.format(removed_switch_cases_num))
 
+    def merge_func_entry_and_exit(self):
+        # For each function call with return there is an edge corresponding to function entry and an edge
+        # corresponding to function exit. Both edges are located at a function call. The second edge can contain an
+        # assigment of result to some variable.
+        # This is good for analysis, but this is redundant for visualization. Let's merge these edges together.
+        edges_to_remove = []
+        for edge in self.trace_iterator():
+            if 'enter' in edge:
+                return_edge = self.get_func_return_edge(edge)
+                if return_edge:
+                    exit_edge = self.next_edge(return_edge)
+                    edges_to_remove.insert(0, exit_edge)
+                    next_to_exit_edge = self.next_edge(exit_edge)
+                    # Do not overwrite source code of function entry with the one of function exit when function is
+                    # called within if statement. In that case there is no useful assigments most likely while source
+                    # code of function exit includes some part of this if statement.
+                    if not next_to_exit_edge \
+                            or 'condition' not in next_to_exit_edge \
+                            or exit_edge['line'] != next_to_exit_edge['line']:
+                        edge['source'] = exit_edge['source']
+
+        for edge_to_remove in edges_to_remove:
+            self.remove_edge_and_target_node(edge_to_remove)
+
     def sanity_checks(self):
         # Check:
         # * branching
