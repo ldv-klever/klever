c856dc7c6fdcb42d57035398acea8291b0a12301
Ilja Zakharov <ilja.zakharov@ispras.ru>
2021-05-11 19:42:42 +0300

    Implement particular checks for testing selective strategy

--- klever/core/vtg/emg/decomposition/modelfactory/test_selective.py
+++ klever/core/vtg/emg/decomposition/modelfactory/test_selective.py
@@ -33,9 +33,13 @@ def test_first(model):
         "cover scenarios": {"c2/p2": {}}
     }
     processes_to_scenarios, models = _obtain_models(model, spec)
-    
-    # todo: Cover all c2p2 scenarios
-    assert False
+
+    # Cover all c2p2 scenarios
+    p2scenarios = processes_to_scenarios['c2/p2']
+    assert len(p2scenarios) == len(models)
+    actions = [m.environment['c2/p2'].actions for m in models]
+    for scenario in p2scenarios:
+        assert scenario.actions in actions
 
 
 def test_second(model):
@@ -45,9 +49,16 @@ def test_second(model):
     }
     processes_to_scenarios, models = _obtain_models(model, spec)
 
-    # todo: Cover all scenarios from c2p1
-    # todo: No savepoints from c2p2
-    assert False
+    # Cover all scenarios from c2p1
+    p1scenarios = processes_to_scenarios['c2/p1']
+    assert len(p1scenarios) == len(models)
+    actions = [m.environment['c2/p1'].actions for m in models]
+    for scenario in p1scenarios:
+        assert scenario.actions in actions
+
+    # No savepoints from c2p2
+    c2p2_withsavepoint = [s for s in processes_to_scenarios['c2/p2'] if s.savepoint].pop()
+    assert all([True if c2p2_withsavepoint.actions != m.entry.actions else False for m in models])
 
 
 def test_third(model):
@@ -58,9 +69,14 @@ def test_third(model):
     }
     processes_to_scenarios, models = _obtain_models(model, spec)
 
-    # todo: Cover only scenarios with read from p2
-    # todo: Not scenarios with a savepoint c2p1s1
-    assert False
+    # Cover only scenarios with read from p2
+    assert len(models) == len([s for s in processes_to_scenarios['c2/p2'] if 'write' not in s.actions])
+    for m in models:
+        assert 'write' not in m.environment['c2/p2'].actions
+
+    # No scenarios with a savepoint c2p1s1
+    c2p1_withsavepoint = [s for s in processes_to_scenarios['c2/p1'] if s.savepoint].pop()
+    assert all([True if c2p1_withsavepoint.actions != m.entry.actions else False for m in models])
 
 
 def _obtain_models(model, specification):
