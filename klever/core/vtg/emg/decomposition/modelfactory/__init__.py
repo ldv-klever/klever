e73808f0355773e53c7758b081a489a7f1c78493
Ilja Zakharov <ilja.zakharov@ispras.ru>
2021-07-14 18:33:46 +0300

    Implement a new interface to work with dependencies

--- klever/core/vtg/emg/decomposition/modelfactory/__init__.py
+++ klever/core/vtg/emg/decomposition/modelfactory/__init__.py
@@ -36,6 +36,112 @@ def remove_process(model, process_name):
     extend_model_name(model, process_name, 'Removed')
 
 
+def process_dependencies(process):
+    """
+    Collect dependnecies (p->actions) for a given process.
+
+    :param process: Process.
+    :return: {p: {actions}}
+    """
+    dependencies_map = dict()
+    for action in (a for a in process.actions.values() if a.require):
+        for name, v in action.require.items():
+            dependencies_map.setdefault(name, set())
+            dependencies_map[name].update(v.get('include', set()))
+
+    return dependencies_map
+
+
+def all_dependencies(processes: set):
+    """
+    Collect dependencies of all processes together.
+
+    :param processes: set of Process objects.
+    :return: {p: {required: {required_actions}}}
+    """
+    dependencies_map = dict()
+    for process in processes:
+        dependencies_map.setdefault(str(process), dict())
+        dependencies_map[str(process)].update(process_dependencies(process))
+
+    return dependencies_map
+
+
+def process_transitive_dependencies(processes: set, process: Process, scenario: Scenario = None):
+    """
+    Collect dependencies transitively of a given process.
+
+    :param processes: Set of Process objects to search dependencies.
+    :param process: Process object.
+    :param scenario: Its scenario that may have less actions.
+    :return: {required: {required_actions}}
+    """
+    assert process in processes or scenario in processes
+    processes_map = {str(p): p for p in processes}
+
+    name = str(process)
+    if not scenario:
+        scenario = process
+    processes_map[name] = scenario
+
+    ret_deps = dict()
+    processed = set()
+    todo = [name]
+    while todo:
+        p_name = todo.pop()
+        p = processes_map[p_name]
+        deps = process_dependencies(p)
+        for required_name in deps:
+            if required_name in processed:
+                raise RecursionError(f'Recursive dependencies for {required_name} calculated for {str(scenario)}')
+            elif required_name in todo:
+                pass
+            else:
+                todo.append(required_name)
+
+            ret_deps.setdefault(required_name, set())
+            ret_deps[required_name].update(deps[required_name])
+    return ret_deps
+
+
+def all_transitive_dependencies(processes: set, selected: set = None):
+    """
+    Collect transitive dependencies for selected processes/scenarios. If selected = None, then all processes are
+    selected.
+
+    :param processes: Set of Process objects.
+    :param selected: Set of Process objects for which is needed to search dependencies.
+    :return: {p: {required: {required_actions}}}
+    """
+    if not selected:
+        selected = processes
+    assert selected.issubset(processes)
+
+    deps = dict()
+    for selected_process in selected:
+        selected_deps = process_transitive_dependencies(processes, selected_process)
+        deps[str(selected_process)] = selected_deps
+    return deps
+
+
+def is_required(dependencies: dict, process: Process, scenario: Scenario = None):
+    """
+    Check that particular process or even its scenario is required by anybody in given dependencies.
+
+    :param dependencies: Dict created by functions defined above.
+    :param process: Process.
+    :param scenario: Scenario object.
+    :return: bool.
+    """
+    name = str(process)
+    actions = set((scenario.actions if scenario else process.actions).keys())
+
+    for entry in dependencies:
+        if name in dependencies[entry] and dependencies[entry][name].issubset(actions):
+            return True
+    return False
+
+
 class ScenarioCollection:
     """
     This is a collection of scenarios. The factory generated the model with processes that have provided keys. If a
@@ -70,6 +176,10 @@ class ScenarioCollection:
                 else:
                     getattr(new, collection)[key] = None
         return new
+
+    @property
+    def defined_processes(self):
+        return {name for name, val in self.environment.items() if val}
 
 
 class Selector:
