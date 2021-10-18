38144a71989045e80bff1607a620d85dd8c19b56
Evgeny Novikov <novikov@ispras.ru>
2021-10-18 16:43:52 +0300

    Fix orphan model
    Yet another example that demonstrates that even trivial test cases are
    better than nothing.

--- presets/jobs/specifications/linux/fs/open.c
+++ presets/jobs/specifications/linux/fs/open.c
@@ -23,9 +23,9 @@ int ldv_vfs_open(const struct path *path, struct file *file)
 	if (ldv_undef_int() && path && path->dentry) {
 		file->f_path = *path;
 
-		f->f_inode = path->dentry->d_inode;
-		f->f_mapping = path->dentry->d_inode->i_mapping;
-		f->f_mode |= FMODE_LSEEK | FMODE_PREAD | FMODE_PWRITE;
+		file->f_inode = path->dentry->d_inode;
+		file->f_mapping = path->dentry->d_inode->i_mapping;
+		file->f_mode |= FMODE_LSEEK | FMODE_PREAD | FMODE_PWRITE;
 
 		return 0;
 	} else
