7bcfa8b8e060e3515b4db58e974e4521256bc7a0
Evgeny Novikov <novikov@ispras.ru>
2022-03-29 19:13:14 +0300

    Consider "int" return type of add_disk() rather than "void"
    This change took place somewhere in ~5.14.

---
+++ klever/cli/descs/linux/testing/environment model specifications/tests/block_device_operations_v.2/add_disk.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 ISP RAS (http://www.ispras.ru)
+ * Ivannikov Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/module.h>
+#include <linux/blkdev.h>
+#include <linux/genhd.h>
+#include <ldv/linux/emg/test_model.h>
+#include <ldv/verifier/nondet.h>
+
+int flip_a_coin;
+
+int ldv_open(struct block_device *bdev, fmode_t mode)
+{
+	ldv_invoke_callback();
+	return ldv_undef_int();
+}
+
+static const struct block_device_operations ops = {
+	.open = ldv_open
+};
+
+struct gendisk disk = {
+    .fops = & ops
+};
+
+static int __init ldv_init(void)
+{
+	int ret = ldv_undef_int();
+
+	flip_a_coin = ldv_undef_int();
+	if (flip_a_coin) {
+		ldv_register();
+		ret = add_disk(& disk);
+		if (ret)
+			ldv_deregister();
+	}
+
+	return ret;
+}
+
+static void __exit ldv_exit(void)
+{
+	if (flip_a_coin) {
+		del_gendisk(& disk);
+		ldv_deregister();
+	}
+}
+
+module_init(ldv_init);
+module_exit(ldv_exit)
+
+MODULE_LICENSE("GPL");
---
+++ klever/cli/descs/linux/testing/environment model specifications/tests/block_device_operations_v.2/disk_invoke.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 ISP RAS (http://www.ispras.ru)
+ * Ivannikov Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/module.h>
+#include <linux/blkdev.h>
+#include <linux/genhd.h>
+#include <ldv/linux/emg/test_model.h>
+#include <ldv/verifier/nondet.h>
+
+int ldv_open(struct block_device *bdev, fmode_t mode)
+{
+	ldv_invoke_reached();
+	return ldv_undef_int();
+}
+
+static const struct block_device_operations ops = {
+	.open = ldv_open
+};
+
+struct gendisk disk = {
+    .fops = & ops
+};
+
+static int __init ldv_init(void)
+{
+	ldv_invoke_test();
+	return add_disk(& disk);
+}
+
+static void __exit ldv_exit(void)
+{
+	del_gendisk(& disk);
+}
+
+module_init(ldv_init);
+module_exit(ldv_exit)
+
+MODULE_LICENSE("GPL");
