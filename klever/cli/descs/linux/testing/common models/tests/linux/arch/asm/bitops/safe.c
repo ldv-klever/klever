c09588d29e72dd37d0712344ab7af87d1ffdcb53
Evgeny Novikov <novikov@ispras.ru>
2022-04-08 21:47:01 +0300

    Extract testing of bitops operations from bitmap test case
    Later this will be also done for appropriate models.

--- klever/cli/descs/linux/testing/common models/tests/linux/arch/asm/bitops/safe.c
+++ klever/cli/descs/linux/testing/common models/tests/linux/arch/asm/bitops/safe.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 ISP RAS (http://www.ispras.ru)
+ * Copyright (c) 2022 ISP RAS (http://www.ispras.ru)
  * Ivannikov Institute for System Programming of the Russian Academy of Sciences
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +16,7 @@
  */
 
 #include <linux/module.h>
-#include <linux/bitmap.h>
+#include <linux/bitops.h>
 #include <ldv/common/test.h>
 
 #define LDV_BIT_MAX 0x100
@@ -26,59 +26,36 @@ static int __init ldv_init(void)
 {
 	unsigned long bit, counter;
 
-	bitmap_set(ldv_bitmap, 0, 10);
-	bitmap_set(ldv_bitmap, 100, 100);
+	set_bit(1, ldv_bitmap);
 
-	counter = 0;
-	for_each_set_bit(bit, ldv_bitmap, LDV_BIT_MAX)
-		counter++;
-
-	if (counter != 110)
+	if (!test_bit(1, ldv_bitmap))
 		ldv_unexpected_memory_safety_error();
 
-	bit = bitmap_find_next_zero_area(ldv_bitmap, LDV_BIT_MAX, 5, 20, 0);
-	if (bit != 10)
+	set_bit(10, ldv_bitmap);
+
+	if (!test_bit(10, ldv_bitmap))
 		ldv_unexpected_memory_safety_error();
 
-	bit = bitmap_find_next_zero_area(ldv_bitmap, LDV_BIT_MAX, 80, 50, 0);
-	if (bit != 200)
+	set_bit(100, ldv_bitmap);
+
+	if (!test_bit(100, ldv_bitmap))
 		ldv_unexpected_memory_safety_error();
 
-	bit = bitmap_find_next_zero_area(ldv_bitmap, LDV_BIT_MAX, 201, 50, 0);
-	if (bit != 201)
-		ldv_unexpected_memory_safety_error();
+	clear_bit(10, ldv_bitmap);
 
-	bit = bitmap_find_next_zero_area(ldv_bitmap, LDV_BIT_MAX, 250, 50, 0);
-	if (bit < LDV_BIT_MAX)
+	if (test_bit(10, ldv_bitmap))
 		ldv_unexpected_memory_safety_error();
 
 	counter = 0;
-	for_each_set_bit(bit, ldv_bitmap, LDV_BIT_MAX)
+	for_each_set_bit(bit, ldv_bitmap, LDV_BIT_MAX) {
+		clear_bit(bit, ldv_bitmap);
 		counter++;
+	}
 
-	if (counter != 110)
+	if (counter != 2)
 		ldv_unexpected_memory_safety_error();
 
-	bitmap_clear(ldv_bitmap, 125, 50);
-
-	counter = 0;
-	for_each_set_bit(bit, ldv_bitmap, LDV_BIT_MAX)
-		counter++;
-
-	if (counter != 60)
-		ldv_unexpected_memory_safety_error();
-
-	bit = bitmap_find_next_zero_area(ldv_bitmap, LDV_BIT_MAX, 80, 50, 0);
-	if (bit != 125)
-		ldv_unexpected_memory_safety_error();
-
-	bitmap_zero(ldv_bitmap, 150);
-
-	counter = 0;
-	for_each_set_bit(bit, ldv_bitmap, LDV_BIT_MAX)
-		counter++;
-
-	if (counter != 8)
+	if (find_next_bit(ldv_bitmap, LDV_BIT_MAX, 0) != LDV_BIT_MAX)
 		ldv_unexpected_memory_safety_error();
 
 	return 0;
