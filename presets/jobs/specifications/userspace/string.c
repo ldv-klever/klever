11a237dc757a8f365a84232e4c54bc4afd4b65c4
Evgeny Novikov <novikov@ispras.ru>
2022-04-18 18:40:05 +0300

    Avoid redundant declaration

--- presets/jobs/specifications/userspace/string.c
+++ presets/jobs/specifications/userspace/string.c
@@ -20,8 +20,6 @@
 #include <ldv/verifier/memory.h>
 #include <ldv/verifier/nondet.h>
 
-extern void *memcpy(void *dest, const void *src, size_t n);
-
 char *ldv_strdup(const char *s);
 char *ldv_strcpy(char *dest, const char *src);
 char *ldv_strncpy(char *dest, const char *src, size_t n);
