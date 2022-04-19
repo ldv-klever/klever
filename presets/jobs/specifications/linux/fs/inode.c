ldv-klever/klever

commit aaa0e241e9179b3802f2bd76f450cda10bca8d1c

Replace remaining calls to mem*() with __VERIFIER_mem*()

presets/jobs/specifications/linux/fs/inode.c
@@ -63,7 +63,7 @@ struct inode *ldv_alloc_inode(struct super_block *sb)
 
 void ldv_inode_init_once(struct inode *inode)
 {
-	memset(inode, 0, sizeof(struct inode));
+	__VERIFIER_memsetinode, 0, sizeof(struct inode));
 }
 
 void ldv_clear_inode(struct inode *inode)

presets/jobs/specifications/linux/drivers/i2c/i2c-core-smbus.c
@@ -36,7 +36,7 @@ s32 ldv_i2c_smbus_read_block_data(u8 *values)
 		/* NOTE "Read" bytes */
 		bytes = ldv_xmalloc(size);
 		/* NOTE Copy read bytes to buffer */
-		memcpy(values, bytes, size);
+		__VERIFIER_memcpy(values, bytes, size);
 		ldv_free(bytes);
 		/* NOTE Return the number of read bytes */
 		return size;

presets/jobs/specifications/linux/fs/dcache/dcache.c
@@ -19,7 +19,7 @@ struct dentry *ldv_d_alloc_pseudo(struct super_block *sb, const struct qstr *nam
 		dentry = ldv_zalloc(sizeof(struct dentry));
 
 		/* Set the string */
-		memcpy(&dentry->d_name, name, sizeof(struct qstr));
+		__VERIFIER_memcpy(&dentry->d_name, name, sizeof(struct qstr));
 
 		dentry->d_sb = sb;
 		dentry->d_parent = dentry;

presets/jobs/specifications/userspace/string.c
@@ -32,7 +32,7 @@ char *ldv_strdup(const char *s)
     char *new;
     if (ldv_undef_int()) {
         new = ldv_xmalloc(sizeof(char) * ldv_strlen(s));
-        memcpy(new, s, ldv_strlen(s));
+        __VERIFIER_memcpy(new, s, ldv_strlen(s));
         return new;
     } else {
         return 0;
@@ -60,6 +60,6 @@ char *ldv_strncpy(char *dest, const char *src, size_t n)
 
 char *ldv_strcpy(char *dest, const char *src)
 {
-    memcpy(dest, src, ldv_strlen(src));
+    __VERIFIER_memcpy(dest, src, ldv_strlen(src));
     return dest;
 }

presets/jobs/specifications/verifier/reference memory.c
@@ -68,7 +68,7 @@ void *ldv_reference_realloc(void *ptr, size_t size)
 		res = malloc(size);
 		ldv_assume(res != NULL);
 		/* TODO: Maybe a better solution exists. */
-		memcpy(res, ptr, size);
+		__VERIFIER_memcpy(res, ptr, size);
 		free(ptr);
 
 		return res;
@@ -128,7 +128,7 @@ void *ldv_reference_calloc_unknown_size(void)
 	if (ldv_undef_int()) {
     	/* NOTE2 Allocate special memory any usage of which will be considered as correct by verification tools (https://tinyurl.com/cu4ycvbx). */
 		res = external_allocated_data();
-		memset(res, 0, sizeof(res));
+		__VERIFIER_memset(res, 0, sizeof(res));
 		ldv_assume(res != NULL);
 		return res;
 	}
@@ -151,4 +151,3 @@ void *ldv_reference_xmalloc_unknown_size(size_t size)
 
 	return res;
 }
-

commit b2723ed5ee920df942a8b1f9907835622f3147a0

Fix typo introduced by commit aaa0e24

presets/jobs/specifications/linux/fs/inode.c
@@ -63,7 +63,7 @@ struct inode *ldv_alloc_inode(struct super_block *sb)
 
 void ldv_inode_init_once(struct inode *inode)
 {
-	__VERIFIER_memsetinode, 0, sizeof(struct inode));
+	__VERIFIER_memset(inode, 0, sizeof(struct inode));
 }
 
 void ldv_clear_inode(struct inode *inode)