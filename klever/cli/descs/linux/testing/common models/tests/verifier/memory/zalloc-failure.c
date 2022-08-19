03572b9902c21dd7b4988508ac79d538fad7d114
Evgeny Novikov <novikov@ispras.ru>
2022-08-19 18:04:24 +0300

    Extend test suite to catch failures of allocation function models
    https://forge.ispras.ru/issues/11836

---
+++ klever/cli/descs/linux/testing/common models/tests/verifier/memory/zalloc-failure.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022 ISP RAS (http://www.ispras.ru)
+ * Ivannikov Institute for System Programming of the Russian Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/module.h>
+#include <ldv/common/test.h>
+
+static int __init ldv_init(void)
+{
+	size_t size = ldv_undef_uint();
+	void *data;
+
+	data = ldv_zalloc(size);;
+
+	if (!data)
+		ldv_expected_error();
+
+	ldv_free(data);
+
+	return 0;
+}
+
+module_init(ldv_init);
+
+MODULE_LICENSE("GPL");
